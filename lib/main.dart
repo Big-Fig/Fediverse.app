fcfd5b660d29deb8a6d40e76d5eeb9a4ae883976
barrett <barrettbreshears@gmail.com>
2019-10-05 10:34:24 -0600

    main update

--- lib/main.dart
+++ lib/main.dart
@@ -25,16 +25,14 @@ void main() async {
 }
 
 class MyApp extends StatelessWidget {
-  final FirebaseMessaging _firebaseMessaging = FirebaseMessaging();
-  FirebaseAnalytics analytics = FirebaseAnalytics();
 
+    FirebaseAnalytics analytics = FirebaseAnalytics();
   final _currentInstance = CurrentInstance.instance;
   final _newInstance = CurrentInstance.newInstance;
 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    config(context);
     print(_currentInstance);
     print(_newInstance);
     SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
@@ -51,34 +49,7 @@ class MyApp extends StatelessWidget {
     );
   }
 
-  config(BuildContext context) {
-    _firebaseMessaging.configure(
-      onMessage: (Map<String, dynamic> message) async {
-        print("onMessage: $message");
-        //   _showItemDialog(message);
-      },
-      onLaunch: (Map<String, dynamic> message) async {
-        print("onLaunch: $message");
-        //  _navigateToItemDetail(message);
-      },
-      onResume: (Map<String, dynamic> message) async {
-        print("onResume: $message");
-        //  _navigateToItemDetail(message);
-      },
-    );
-
-    _firebaseMessaging.requestNotificationPermissions(
-        const IosNotificationSettings(sound: true, badge: true, alert: true));
-    _firebaseMessaging.onIosSettingsRegistered
-        .listen((IosNotificationSettings settings) {
-      print("Settings registered: $settings");
-    });
-    _firebaseMessaging.getToken().then((String token) {
-      assert(token != null);
-      print("Token");
-      print(token);
-    });
-  }
+  
 }
 
 Future<dynamic> myBackgroundMessageHandler(Map<String, dynamic> message) async {
