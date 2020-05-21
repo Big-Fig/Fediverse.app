c0fe2b9a7bcc8e75a4b7bce1e9a77eddb219aa4f
Yevhenii Zapletin <zapletin.yevhenii@gmail.com>
2020-05-21 21:52:20 +0300

    improve UI: fix chat messages list

--- lib/app/date/date_utils.dart
+++ lib/app/date/date_utils.dart
@@ -7,5 +7,12 @@ class DateUtils {
   }
 
   static DateTime dayStartOf(DateTime dateTime) =>
-      DateTime(dateTime.year, dateTime.month, dateTime.day);
+      DateTime(dateTime.year, dateTime.month, dateTime.day, 0, 0, 0, 0, 0);
+  static DateTime minuteStartOf(DateTime dateTime) => DateTime(dateTime.year,
+      dateTime.month, dateTime.day, dateTime.hour, dateTime.minute, 0, 0, 0);
+
+  static bool isSameDay(DateTime date1, DateTime date2) =>
+      dayStartOf(date1).compareTo(dayStartOf(date2)) == 0;
+  static bool isSameMinute(DateTime date1, DateTime date2) =>
+      minuteStartOf(date1).compareTo(minuteStartOf(date2)) == 0;
 }
