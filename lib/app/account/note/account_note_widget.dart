93ad5e9fd391536bcbe4b58408c559722387feff
Yevhenii Zapletin <zapletin.yevhenii@gmail.com>
2020-05-22 17:47:09 +0300

    improve UI: account details note

--- lib/app/account/note/account_note_widget.dart
+++ lib/app/account/note/account_note_widget.dart
@@ -1,5 +1,6 @@
 import 'package:fedi/app/account/account_bloc.dart';
 import 'package:fedi/app/html/html_text_widget.dart';
+import 'package:fedi/app/ui/fedi_colors.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:url_launcher/url_launcher.dart';
@@ -15,22 +16,23 @@ class AccountNoteWidget extends StatelessWidget {
           var note = snapshot.data;
 
           if (note?.isNotEmpty == true) {
-            return Container(
-              color: Colors.blue[100],
-              child: Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: HtmlTextWidget(
-                  data: note,
-                  onLinkTap: (String url) async {
-                    if (await canLaunch(url)) {
-                      await launch(url);
-                    }
-                  },
-                ),
+            return Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: HtmlTextWidget(
+                data: note,
+                shrinkWrap: true,
+                fontWeight: FontWeight.w500,
+                fontSize: 16.0,
+                color: FediColors.darkGrey,
+                onLinkTap: (String url) async {
+                  if (await canLaunch(url)) {
+                    await launch(url);
+                  }
+                },
               ),
             );
           } else {
-            return SizedBox.shrink();
+            return const SizedBox.shrink();
           }
         });
   }
