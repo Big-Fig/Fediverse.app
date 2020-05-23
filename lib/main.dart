0b547dd76eb095315441d2803625f9d486605b5c
Yevhenii Zapletin <zapletin.yevhenii@gmail.com>
2020-05-23 16:59:56 +0300

    don't sent crashlytics errors in debug mode

--- lib/main.dart
+++ lib/main.dart
@@ -94,7 +94,6 @@ void main() async {
   // This is only to be used for confirming that reports are being
   // submitted as expected. It is not intended to be used for everyday
   // development.
-  Crashlytics.instance.enableInDevMode = true;
 
   // Pass all uncaught errors from the framework to Crashlytics.
   FlutterError.onError = Crashlytics.instance.recordFlutterError;
