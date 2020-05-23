9a8bf3068f753f3b36b7005e00d0e910c2cef628
Yevhenii Zapletin <zapletin.yevhenii@gmail.com>
2020-05-23 16:59:36 +0300

    fix fedi theme colors

--- lib/app/ui/fedi_theme.dart
+++ lib/app/ui/fedi_theme.dart
@@ -1,10 +1,23 @@
 import 'package:fedi/app/ui/fedi_colors.dart';
 import 'package:flutter/material.dart';
 
+Map<int, Color> _primaryColorSwatch = {
+  50: FediColors.primaryColor.withOpacity(0.1),
+  100: FediColors.primaryColor.withOpacity(0.2),
+  200: FediColors.primaryColor.withOpacity(0.3),
+  300: FediColors.primaryColor.withOpacity(0.4),
+  400: FediColors.primaryColor.withOpacity(0.5),
+  500: FediColors.primaryColor.withOpacity(0.6),
+  600: FediColors.primaryColor.withOpacity(0.7),
+  700: FediColors.primaryColor.withOpacity(0.8),
+  800: FediColors.primaryColor.withOpacity(0.9),
+  900: FediColors.primaryColor.withOpacity(1.0),
+};
+
 final ThemeData fediTheme = ThemeData(
   fontFamily: "Rubik",
   primarySwatch: FediColors.primaryColor,
-  primaryColor: FediColors.primaryColor,
+  primaryColor: MaterialColor(0xff00BCEC, _primaryColorSwatch),
   primaryColorDark: FediColors.primaryColorDark,
   accentColor: FediColors.secondaryColor,
 );
