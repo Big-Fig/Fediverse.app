634d050dbd280ac44f7b2a14a1e2af57b26c6b71
Yevhenii Zapletin <zapletin.yevhenii@gmail.com>
2020-05-24 23:25:23 +0300

    improve UI: fix current instance loading screen

--- lib/app/auth/instance/current/context/loading/current_auth_instance_context_loading_bloc.dart
+++ lib/app/auth/instance/current/context/loading/current_auth_instance_context_loading_bloc.dart
@@ -1,9 +1,11 @@
 import 'package:fedi/app/auth/instance/current/context/loading/current_auth_instance_context_loading_model.dart';
+import 'package:fedi/async/loading/init/async_init_loading_bloc.dart';
 import 'package:fedi/disposable/disposable.dart';
 import 'package:flutter/widgets.dart';
 import 'package:provider/provider.dart';
 
-abstract class ICurrentAuthInstanceContextLoadingBloc extends Disposable {
+abstract class ICurrentAuthInstanceContextLoadingBloc
+    implements IAsyncInitLoadingBloc, Disposable {
   static ICurrentAuthInstanceContextLoadingBloc of(BuildContext context,
           {bool listen = true}) =>
       Provider.of<ICurrentAuthInstanceContextLoadingBloc>(context,
--- lib/app/auth/instance/current/context/loading/current_auth_instance_context_loading_widget.dart
+++ lib/app/auth/instance/current/context/loading/current_auth_instance_context_loading_widget.dart
@@ -3,7 +3,9 @@ import 'package:fedi/app/account/my/action/my_account_action_list_bottom_sheet_d
 import 'package:fedi/app/account/my/my_account_bloc.dart';
 import 'package:fedi/app/auth/instance/current/context/loading/current_auth_instance_context_loading_bloc.dart';
 import 'package:fedi/app/auth/instance/current/context/loading/current_auth_instance_context_loading_model.dart';
+import 'package:fedi/app/ui/button/text/fedi_grey_filled_text_button.dart';
 import 'package:fedi/app/ui/fedi_colors.dart';
+import 'package:fedi/async/loading/init/async_init_loading_widget.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:logging/logging.dart';
@@ -22,62 +24,96 @@ class CurrentAuthInstanceContextLoadingWidget extends StatelessWidget {
 
     _logger.finest(() => "build");
 
-    return Container(
-      decoration: BoxDecoration(color: FediColors.primaryColor),
-      child: StreamBuilder<CurrentAuthInstanceContextLoadingState>(
-          stream: currentInstanceContextLoadingBloc.stateStream.distinct(),
-          initialData: currentInstanceContextLoadingBloc.state,
-          builder: (context, snapshot) {
-            var state = snapshot.data;
-            _logger.finest(() => "state $state");
+    return Scaffold(
+      body: AsyncInitLoadingWidget(
+        asyncInitLoadingBloc: currentInstanceContextLoadingBloc,
+        loadingFinishedBuilder: (BuildContext context) {
+          return StreamBuilder<CurrentAuthInstanceContextLoadingState>(
+              stream: currentInstanceContextLoadingBloc.stateStream.distinct(),
+              initialData: currentInstanceContextLoadingBloc.state,
+              builder: (context, snapshot) {
+                var state = snapshot.data;
+                _logger.finest(() => "state $state");
 
-            switch (state) {
-              case CurrentAuthInstanceContextLoadingState.loading:
-                var myAccountBloc = IMyAccountBloc.of(context, listen: true);
+                switch (state) {
+                  case CurrentAuthInstanceContextLoadingState.loading:
+                    var myAccountBloc =
+                        IMyAccountBloc.of(context, listen: true);
 
-                return Scaffold(
-                  body: Center(
-                      child: Text(
-                    AppLocalizations.of(context).tr(
-                        "app.auth.instance.current.context.loading.loading"
-                        ".content",
-                        args: [myAccountBloc.instance.userAtHost]),
-                    style: TextStyle(color: Colors.white),
-                  )),
-                );
-                break;
-              case CurrentAuthInstanceContextLoadingState.localCacheExist:
-                return child;
-              case CurrentAuthInstanceContextLoadingState
-                  .cantFetchAndLocalCacheNotExist:
-                return Column(
-                  children: <Widget>[
-                    Text(AppLocalizations.of(context).tr(
-                        "app.auth.instance.current.context.loading.cant_load"
-                        ".content")),
-                    RaisedButton(
-                      child: Text(AppLocalizations.of(context).tr(
-                          "app.auth.instance.current.context.loading.cant_load"
-                          ".action.refresh")),
-                      onPressed: () {
-                        currentInstanceContextLoadingBloc.refresh();
-                      },
-                    ),
-                    RaisedButton(
-                      child: Text(AppLocalizations.of(context).tr(
-                          "app.auth.instance.current.context.loading.cant_load"
-                          ".action.choose_different_account")),
-                      onPressed: () {
-                        showMyAccountActionListBottomSheetDialog(context);
-                      },
-                    ),
-                  ],
-                );
-                break;
-            }
+                    return Scaffold(
+                      backgroundColor: FediColors.primaryColor,
+                      body: SafeArea(
+                        child: Center(
+                            child: Text(
+                          AppLocalizations.of(context).tr(
+                              "app.auth.instance.current.context.loading.loading"
+                              ".content",
+                              args: [myAccountBloc.instance.userAtHost]),
+                          style: TextStyle(color: Colors.white),
+                        )),
+                      ),
+                    );
+                    break;
+                  case CurrentAuthInstanceContextLoadingState.localCacheExist:
+                    return child;
+                  case CurrentAuthInstanceContextLoadingState
+                      .cantFetchAndLocalCacheNotExist:
+                    return Scaffold(
+                      backgroundColor: FediColors.primaryColor,
+                      body: SafeArea(
+                        child: Center(
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            crossAxisAlignment: CrossAxisAlignment.center,
+                            children: <Widget>[
+                              Padding(
+                                padding: const EdgeInsets.all(16.0),
+                                child: Text(
+                                  AppLocalizations.of(context).tr(
+                                      "app.auth.instance.current.context.loading.cant_load"
+                                      ".content"),
+                                  style: TextStyle(
+                                      color: FediColors.white,
+                                      fontWeight: FontWeight.w500),
+                                ),
+                              ),
+                              Padding(
+                                padding: const EdgeInsets.all(8.0),
+                                child: FediGreyFilledTextButton(
+                                  AppLocalizations.of(context).tr(
+                                      "app.auth.instance.current.context.loading.cant_load"
+                                      ".action.refresh"),
+                                  onPressed: () {
+                                    currentInstanceContextLoadingBloc.refresh();
+                                  },
+                                  textColor: FediColors.darkGrey,
+                                ),
+                              ),
+                              Padding(
+                                padding: const EdgeInsets.all(8.0),
+                                child: FediGreyFilledTextButton(
+                                  AppLocalizations.of(context).tr(
+                                      "app.auth.instance.current.context.loading.cant_load"
+                                      ".action.choose_different_account"),
+                                  onPressed: () {
+                                    showMyAccountActionListBottomSheetDialog(
+                                        context);
+                                  },
+                                  textColor: FediColors.darkGrey,
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    );
+                    break;
+                }
 
-            throw "Invalid state $state";
-          }),
+                throw "Invalid state $state";
+              });
+        },
+      ),
     );
   }
 }
