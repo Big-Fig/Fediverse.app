89ff327b159ff80ad9a6e56de9562bb2788afba2
Yevhenii Zapletin <zapletin.yevhenii@gmail.com>
2020-05-26 23:50:17 +0300

    don't collapse small status content with html code

--- lib/app/html/html_text_helper.dart
+++ lib/app/html/html_text_helper.dart
@@ -4,6 +4,9 @@ import 'package:html/parser.dart';
 
 class HtmlTextHelper {
   static String extractRawStringFromHtmlString(String htmlText) {
+    if(htmlText == null) {
+      return null;
+    }
     var document = parse(htmlText);
 
     String parsedString = parse(document.body.text).documentElement.text;
