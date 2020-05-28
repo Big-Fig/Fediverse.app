f4edec7d46cf29f323b48c43ac1d699fee7c0b01
Yevhenii Zapletin <zapletin.yevhenii@gmail.com>
2020-05-28 17:12:04 +0300

    improve UI: status card UI

--- lib/app/status/card/status_card_widget.dart
+++ lib/app/status/card/status_card_widget.dart
@@ -1,10 +1,13 @@
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:fedi/app/status/status_bloc.dart';
+import 'package:fedi/app/ui/fedi_colors.dart';
 import 'package:fedi/app/url/url_helper.dart';
 import 'package:fedi/pleroma/card/pleroma_card_model.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
+final _cardImageSize = 114.0;
+var _cardBorderRadius = 8.0;
 
 class StatusCardWidget extends StatelessWidget {
   @override
@@ -22,82 +25,23 @@ class StatusCardWidget extends StatelessWidget {
           }
 
           return Padding(
-            padding: const EdgeInsets.symmetric(vertical:8.0),
-            child: GestureDetector(
-              onTap: () async {
-                var url = card.url;
-                await UrlHelper.handleUrlClick(context, url);
-              },
-              child: ConstrainedBox(
-                constraints: BoxConstraints(
-                  minHeight: 0,
-                  maxHeight: 135.0,
-                ),
-                child: Container(
-                  decoration: BoxDecoration(
-                    border: Border.all(
-                      color: Colors.black38,
-                      width: 1,
-                    ),
-                    borderRadius: BorderRadius.circular(3),
-                  ),
-                  child: Padding(
-                    padding: EdgeInsets.all(3),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.start,
-                      mainAxisSize: MainAxisSize.max,
-                      children: <Widget>[
-                        if (card.image != null)
-                          AspectRatio(
-                            aspectRatio: 1,
-                            child: CachedNetworkImage(
-                              imageUrl: card.image,
-                              fit: BoxFit.cover,
-                              placeholder: (context, url) => Padding(
-                                padding: const EdgeInsets.all(20.0),
-                                child: CircularProgressIndicator(),
-                              ),
-                              errorWidget: (context, url, error) =>
-                                  Icon(Icons.error),
-                            ),
-                          ),
-                        Expanded(
-                          child: Padding(
-                            padding: EdgeInsets.only(left: 3),
-                            child: Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              mainAxisAlignment: MainAxisAlignment.start,
-                              mainAxisSize: MainAxisSize.min,
-                              children: <Widget>[
-                                if (card.providerName != null)
-                                  Text(
-                                    card.providerName,
-                                    style: TextStyle(
-                                        color: Colors.black45, fontSize: 13),
-                                    overflow: TextOverflow.ellipsis,
-                                  ),
-                                if (card.title != null)
-                                  Text(
-                                    card.title,
-                                    maxLines: 2,
-                                    style: TextStyle(
-                                        color: Colors.black,
-                                        fontWeight: FontWeight.bold),
-                                    overflow: TextOverflow.ellipsis,
-                                  ),
-                                if (card.description != null)
-                                  Flexible(
-                                    child: Text(
-                                      card.description,
-                                      overflow: TextOverflow.clip,
-                                    ),
-                                  ),
-                              ],
-                            ),
-                          ),
-                        )
-                      ],
-                    ),
+            padding: const EdgeInsets.symmetric(vertical: 12.0),
+            child: Container(
+              height: _cardImageSize,
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(_cardBorderRadius),
+                child: GestureDetector(
+                  onTap: () async {
+                    var url = card.url;
+                    await UrlHelper.handleUrlClick(context, url);
+                  },
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    mainAxisSize: MainAxisSize.max,
+                    children: <Widget>[
+                      if (card.image != null) buildImage(card),
+                      buildContent(card)
+                    ],
                   ),
                 ),
               ),
@@ -106,5 +50,75 @@ class StatusCardWidget extends StatelessWidget {
         });
   }
 
-  const StatusCardWidget();
+  Widget buildContent(IPleromaCard card) => Expanded(
+        child: Container(
+          decoration: BoxDecoration(
+              borderRadius: BorderRadius.only(
+                  topRight: Radius.circular(_cardBorderRadius),
+                  bottomRight: Radius.circular(_cardBorderRadius)),
+              border: Border.all(color: FediColors.ultraLightGrey)),
+          child: Padding(
+            padding: EdgeInsets.all(12),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.start,
+              mainAxisSize: MainAxisSize.min,
+              children: <Widget>[
+                if (card.providerName != null) buildProviderText(card),
+                if (card.title != null) buildTitleText(card),
+                if (card.description != null) buildDescriptionText(card),
+              ],
+            ),
+          ),
+        ),
+      );
+
+  Widget buildDescriptionText(IPleromaCard card) => Expanded(
+        child: Text(card.description,
+            overflow: TextOverflow.ellipsis,
+            style: TextStyle(
+              color: FediColors.darkGrey,
+              fontSize: 15.0,
+              height: 1.5,
+            )),
+      );
+
+  Text buildTitleText(IPleromaCard card) {
+    return Text(
+      card.title,
+      maxLines: 2,
+      style: TextStyle(
+          color: FediColors.darkGrey,
+          fontSize: 16.0,
+          height: 1.5,
+          fontWeight: FontWeight.w500),
+      overflow: TextOverflow.ellipsis,
+    );
+  }
+
+  Text buildProviderText(IPleromaCard card) {
+    return Text(
+      card.providerName,
+      style: TextStyle(
+        color: FediColors.grey,
+        fontSize: 12,
+        height: 1.5,
+      ),
+      overflow: TextOverflow.ellipsis,
+    );
+  }
+
+  Widget buildImage(IPleromaCard card) => CachedNetworkImage(
+        width: _cardImageSize,
+        height: _cardImageSize,
+        imageUrl: card.image,
+        fit: BoxFit.cover,
+        placeholder: (context, url) => Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: CircularProgressIndicator(),
+        ),
+        errorWidget: (context, url, error) => Icon(Icons.error),
+      );
+
+  StatusCardWidget();
 }
