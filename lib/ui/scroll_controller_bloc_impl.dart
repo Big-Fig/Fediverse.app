63273b2562330ca5789b7ba72dd36091ac425a0f
Yevhenii Zapletin <zapletin.yevhenii@gmail.com>
2020-06-02 21:32:48 +0300

    add long scroll support

--- lib/ui/scroll_controller_bloc_impl.dart
+++ lib/ui/scroll_controller_bloc_impl.dart
@@ -2,27 +2,47 @@ import 'package:fedi/disposable/disposable_owner.dart';
 import 'package:fedi/ui/scroll_controller_bloc.dart';
 import 'package:flutter/rendering.dart';
 import 'package:flutter/widgets.dart';
+import 'package:logging/logging.dart';
 import 'package:rxdart/rxdart.dart';
+
+var _logger = Logger("scroll_controller_bloc_impl.dart");
 
 class ScrollControllerBloc extends DisposableOwner
     implements IScrollControllerBloc {
   @override
   final ScrollController scrollController;
 
+  DateTime lastDirectionSwitchDateTime;
+
   ScrollControllerBloc({@required this.scrollController})
       : scrollDirectionSubject = BehaviorSubject.seeded(null) {
     addDisposable(subject: scrollDirectionSubject);
     var listener = () {
-      scrollDirectionSubject.add(scrollController.position.userScrollDirection);
+      var previousScrollDirection = scrollDirection;
+      var currentScrollDirection =
+          scrollController.position.userScrollDirection;
+
+      _logger.finest(() => "previous $previousScrollDirection "
+          "current $currentScrollDirection");
+      var now = DateTime.now();
+      if (currentScrollDirection != previousScrollDirection) {
+        lastDirectionSwitchDateTime = now;
+        scrollDirectionSubject.add(currentScrollDirection);
+        longScrollDirectionSubject.add(null);
+      } else {
+        var difference = now.difference(lastDirectionSwitchDateTime);
+        _logger
+            .finest(() => "difference $difference");
+        if (difference.inMilliseconds > 1000) {
+          longScrollDirectionSubject.add(currentScrollDirection);
+        }
+      }
     };
     scrollController.addListener(listener);
     addDisposable(custom: () {
       try {
-
-      scrollController.removeListener(listener);
-      } catch(e) {
-
-      }
+        scrollController.removeListener(listener);
+      } catch (e) {}
     });
   }
 
@@ -34,6 +54,15 @@ class ScrollControllerBloc extends DisposableOwner
 
   @override
   ScrollDirection get scrollDirection => scrollDirectionSubject.value;
+  BehaviorSubject<ScrollDirection> longScrollDirectionSubject =
+      BehaviorSubject.seeded(null);
+
+  @override
+  Stream<ScrollDirection> get longScrollDirectionStream =>
+      longScrollDirectionSubject.stream;
+
+  @override
+  ScrollDirection get longScrollDirection => longScrollDirectionSubject.value;
 
   @override
   void scrollToTop() {
