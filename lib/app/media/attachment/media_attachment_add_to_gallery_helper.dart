984f59d943606e17caa6da5a14ad34fe5cb3e1ee
Yevhenii Zapletin <zapletin.yevhenii@gmail.com>
2020-06-04 23:55:56 +0300

    add logging to saving media attachment

--- lib/app/media/attachment/media_attachment_add_to_gallery_helper.dart
+++ lib/app/media/attachment/media_attachment_add_to_gallery_helper.dart
@@ -2,8 +2,15 @@ import 'package:fedi/pleroma/media/attachment/pleroma_media_attachment_model.dar
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:gallery_saver/gallery_saver.dart';
+import 'package:logging/logging.dart';
+
+var _logger = Logger("media_attachment_add_to_gallery_helper.dart");
 
 Future<bool> addMediaAttachmentToGallery(
         {@required BuildContext context,
-        @required IPleromaMediaAttachment mediaAttachment}) =>
-    GallerySaver.saveImage(mediaAttachment.url);
+        @required IPleromaMediaAttachment mediaAttachment}) async {
+        _logger.finest(() => "addMediaAttachmentToGallery start");
+  var saved = await GallerySaver.saveImage(mediaAttachment.url);
+        _logger.finest(() => "addMediaAttachmentToGallery saved =  $saved");
+  return saved;
+}
