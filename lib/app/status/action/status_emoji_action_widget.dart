fb79f67a94c0c14709c862088c6ecc59569551f4
Yevhenii Zapletin <zapletin.yevhenii@gmail.com>
2020-06-15 14:12:06 +0300

    add error dialog on "can't add emoji reaction"

--- lib/app/status/action/status_emoji_action_widget.dart
+++ lib/app/status/action/status_emoji_action_widget.dart
@@ -1,7 +1,10 @@
+import 'package:easy_localization/easy_localization.dart';
 import 'package:fedi/app/status/emoji_reaction/status_emoji_reaction_picker_widget.dart';
 import 'package:fedi/app/status/status_bloc.dart';
 import 'package:fedi/app/ui/fedi_colors.dart';
 import 'package:fedi/app/ui/fedi_icons.dart';
+import 'package:fedi/dialog/alert/simple_alert_dialog.dart';
+import 'package:fedi/dialog/async/async_dialog.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
@@ -9,37 +12,54 @@ class StatusEmojiActionWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     var statusBloc = IStatusBloc.of(context, listen: false);
-    return Row(children: [
-      IconButton(
-        color: FediColors.darkGrey,
-        iconSize: 20.0,
-        icon: Icon(FediIcons.emoji),
-        onPressed: () {
-          _showEmojiPicker(context, statusBloc);
-        },),
-      StreamBuilder<int>(
-          stream: statusBloc.reblogPlusOriginalEmojiReactionsCountStream,
-          initialData: statusBloc.reblogPlusOriginalEmojiReactionsCount,
-          builder: (context, snapshot) {
-            var favouritesCount = snapshot.data;
-            if (favouritesCount == null) {
-              return SizedBox.shrink();
-            }
-            return GestureDetector(onTap: () {
-              _showEmojiPicker(context, statusBloc);
-            },
-                child: Text(favouritesCount.toString(), style: TextStyle(
-                  fontSize: 12, color: FediColors.darkGrey,),));
-          }),
-    ],);
+    return Row(
+      children: [
+        IconButton(
+          color: FediColors.darkGrey,
+          iconSize: 20.0,
+          icon: Icon(FediIcons.emoji),
+          onPressed: () {
+            _showEmojiPicker(context, statusBloc);
+          },
+        ),
+        StreamBuilder<int>(
+            stream: statusBloc.reblogPlusOriginalEmojiReactionsCountStream,
+            initialData: statusBloc.reblogPlusOriginalEmojiReactionsCount,
+            builder: (context, snapshot) {
+              var favouritesCount = snapshot.data;
+              if (favouritesCount == null) {
+                return SizedBox.shrink();
+              }
+              return GestureDetector(
+                  onTap: () {
+                    _showEmojiPicker(context, statusBloc);
+                  },
+                  child: Text(
+                    favouritesCount.toString(),
+                    style: TextStyle(
+                      fontSize: 12,
+                      color: FediColors.darkGrey,
+                    ),
+                  ));
+            }),
+      ],
+    );
   }
 
   void _showEmojiPicker(BuildContext context, IStatusBloc statusBloc) {
     showEmojiPickerModalPopup(context,
         emojiReactionSelectedCallback: (String emojiName, String emoji) {
-          statusBloc.toggleEmojiReaction(emoji: emoji);
-        });
+      doAsyncOperationWithDialog(
+          context: context,
+          asyncCode: () => statusBloc.toggleEmojiReaction(emoji: emoji),
+          errorAlertDialogBuilders: [
+            (BuildContext context, dynamic error) => SimpleAlertDialog(
+                context: context,
+                title: "app.status.emoji.error.cant_add.dialog.title".tr(),
+                content:
+                    "app.status.emoji.error.cant_add.dialog.content".tr
+                      (args: [error.toString()]))
+          ]);
+    });
   }
-
-
 }
