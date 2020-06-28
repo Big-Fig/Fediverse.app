595a5d8663fb158f167cf66da140a1768408c12a
Yevhenii Zapletin <zapletin.yevhenii@gmail.com>
2020-06-28 14:39:14 +0300

    improve attached UI

--- lib/app/media/attachment/media_attachment_non_media_item_widget.dart
+++ lib/app/media/attachment/media_attachment_non_media_item_widget.dart
@@ -20,90 +20,99 @@ class MediaAttachmentNonMediaItemWidget extends StatelessWidget {
     var fileExtension = extension(filePath);
     fileExtension = fileExtension?.replaceAll(".", "");
 
-    return Stack(
-      children: [
-        Opacity(
-          opacity: opacity,
-          child: Container(
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.all(Radius.circular(8.0)),
-              border: Border.all(color: FediColors.ultraLightGrey),
-            ),
-            child: Row(
-              children: [
-                Container(
-                  width: 70.0,
-                  height: 70.0,
-                  decoration: BoxDecoration(
-                    border: Border(
-                      right: BorderSide(
-                        width: 1,
-                        color: FediColors.ultraLightGrey,
-                      ),
-                    ),
-                  ),
-                  child: Center(
-                      child: Padding(
-                    padding: const EdgeInsets.all(16.0),
-                    child: Container(
-                      decoration: BoxDecoration(
-                          border: Border.all(
-                        width: 1,
-                        color: FediColors.darkGrey,
-                      )),
-                      child: Center(
-                          child: AutoSizeText(
-                        fileExtension?.toUpperCase(),
-                        overflow: TextOverflow.ellipsis,
-                        style: TextStyle(color: FediColors.darkGrey),
-                      )),
-                    ),
-                  )),
+    if (fileExtension?.isNotEmpty != true) {
+      fileExtension = "?";
+    }
+
+    return Center(
+      child: Container(
+        height: 70,
+        child: Stack(
+          children: [
+            Opacity(
+              opacity: opacity,
+              child: Container(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.all(Radius.circular(8.0)),
+                  border: Border.all(color: FediColors.ultraLightGrey),
                 ),
-                Expanded(
-                  child: Padding(
-                    padding: const EdgeInsets.all(12.0),
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(
-                          fileExtension?.toUpperCase(),
-                          overflow: TextOverflow.ellipsis,
-                          style: TextStyle(
-                              color: FediColors.grey,
-                              height: 1.5,
-                              fontSize: 12),
+                child: Row(
+                  children: [
+                    Container(
+                      width: 70.0,
+                      height: 70.0,
+                      decoration: BoxDecoration(
+                        border: Border(
+                          right: BorderSide(
+                            width: 1,
+                            color: FediColors.ultraLightGrey,
+                          ),
                         ),
-                        Text(
-                          fileName,
-                          overflow: TextOverflow.ellipsis,
-                          style: TextStyle(
-                              color: FediColors.darkGrey,
-                              height: 1.5,
-                              fontSize: 15),
+                      ),
+                      child: Center(
+                          child: Padding(
+                        padding: const EdgeInsets.all(16.0),
+                        child: Container(
+                          decoration: BoxDecoration(
+                              border: Border.all(
+                            width: 1,
+                            color: FediColors.darkGrey,
+                          )),
+                          child: Center(
+                              child: AutoSizeText(
+                            fileExtension?.toUpperCase(),
+                            overflow: TextOverflow.ellipsis,
+                            style: TextStyle(color: FediColors.darkGrey),
+                          )),
                         ),
-                      ],
+                      )),
                     ),
-                  ),
-                )
-              ],
+                    Expanded(
+                      child: Padding(
+                        padding: const EdgeInsets.all(12.0),
+                        child: Column(
+//                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              fileExtension?.toUpperCase(),
+                              overflow: TextOverflow.ellipsis,
+                              style: TextStyle(
+                                  color: FediColors.grey,
+                                  height: 1.5,
+                                  fontSize: 12),
+                            ),
+                            Text(
+                              fileName,
+                              overflow: TextOverflow.ellipsis,
+                              style: TextStyle(
+                                  color: FediColors.darkGrey,
+                                  height: 1.5,
+                                  fontSize: 15),
+                            ),
+                          ],
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+              ),
             ),
-          ),
+            if (actionsWidget != null)
+              Positioned(
+                top: 0.0,
+                bottom: 0.0,
+                right: 16.0,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    actionsWidget,
+                  ],
+                ),
+              )
+          ],
         ),
-        if (actionsWidget != null)
-          Positioned(
-            top: 0.0,
-            bottom: 0.0,
-            right: 16.0,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                actionsWidget,
-              ],
-            ),
-          )
-      ],
+      ),
     );
   }
 }
