96aee0886793cf18affa9c4be99ad0d18cb5a702
Yevhenii Zapletin <zapletin.yevhenii@gmail.com>
2020-07-02 23:39:07 +0300

    fix account note UI

--- lib/app/account/note/account_note_widget.dart
+++ lib/app/account/note/account_note_widget.dart
@@ -1,14 +1,12 @@
 import 'package:fedi/app/account/account_bloc.dart';
 import 'package:fedi/app/emoji/emoji_text_model.dart';
 import 'package:fedi/app/emoji/emoji_text_widget.dart';
-import 'package:fedi/app/html/html_text_widget.dart';
-import 'package:fedi/app/url/url_helper.dart';
+import 'package:fedi/app/ui/fedi_colors.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class AccountNoteWidget extends StatelessWidget {
   final Color color;
-
 
   const AccountNoteWidget({@required this.color});
 
@@ -19,24 +17,26 @@ class AccountNoteWidget extends StatelessWidget {
         stream: accountBloc.noteEmojiTextStream,
         initialData: accountBloc.noteEmojiText,
         builder: (context, snapshot) {
-
           var noteEmojiText = snapshot.data;
 
           if (noteEmojiText?.text?.isNotEmpty != true) {
             return SizedBox.shrink();
           }
 
-          return EmojiTextWidget(
-            textAlign: TextAlign.center,
-            emojiText: noteEmojiText,
-            textStyle: TextStyle(
-              fontSize: 16.0,
-              height: 1.5,
-              fontWeight: FontWeight.w500,
+          return Padding(
+            padding: const EdgeInsets.only(bottom: 8.0),
+            child: EmojiTextWidget(
+              textAlign: TextAlign.center,
+              emojiText: noteEmojiText,
+              textStyle: TextStyle(
+                fontSize: 16.0,
+                height: 1.5,
+                color: color,
+                fontWeight: FontWeight.w500,
+              ),
+              textOverflow: TextOverflow.visible,
             ),
-            textOverflow: TextOverflow.visible,
           );
         });
   }
-
 }
