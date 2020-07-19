651ff5285d329935c4f32916647d446292fae83a
Yevhenii Zapletin <zapletin.yevhenii@gmail.com>
2020-07-19 20:58:39 +0300

    revert main. Overlay support not bugged

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,7 @@
 import 'package:easy_localization/easy_localization.dart';
 import 'package:fedi/app/account/my/my_account_bloc.dart';
-import 'package:fedi/app/analytics/analytics_service.dart';
+import 'package:fedi/app/auth/host/auth_host_bloc_impl.dart';
 import 'package:fedi/app/auth/instance/auth_instance_model.dart';
-import 'package:fedi/app/auth/instance/current/context/current_auth_instance_context_bloc.dart';
 import 'package:fedi/app/auth/instance/current/context/current_auth_instance_context_bloc_impl.dart';
 import 'package:fedi/app/auth/instance/current/context/loading/current_auth_instance_context_loading_bloc.dart';
 import 'package:fedi/app/auth/instance/current/context/loading/current_auth_instance_context_loading_bloc_impl.dart';
@@ -11,6 +10,7 @@ import 'package:fedi/app/auth/instance/current/current_auth_instance_bloc.dart';
 import 'package:fedi/app/auth/instance/join/from_scratch/from_scratch_join_auth_instance_page.dart';
 import 'package:fedi/app/auth/instance/join/join_auth_instance_bloc.dart';
 import 'package:fedi/app/auth/instance/join/join_auth_instance_bloc_impl.dart';
+import 'package:fedi/app/context/app_context_bloc.dart';
 import 'package:fedi/app/context/app_context_bloc_impl.dart';
 import 'package:fedi/app/home/home_bloc.dart';
 import 'package:fedi/app/home/home_bloc_impl.dart';
@@ -18,13 +18,14 @@ import 'package:fedi/app/home/home_model.dart';
 import 'package:fedi/app/home/home_page.dart';
 import 'package:fedi/app/init/init_bloc.dart';
 import 'package:fedi/app/init/init_bloc_impl.dart';
-import 'package:fedi/app/localization/localization_loader.dart';
 import 'package:fedi/app/splash/splash_page.dart';
-import 'package:fedi/app/ui/fedi_colors.dart';
 import 'package:fedi/app/ui/fedi_theme.dart';
 import 'package:fedi/async/loading/init/async_init_loading_model.dart';
 import 'package:fedi/disposable/disposable_provider.dart';
 import 'package:fedi/pleroma/instance/pleroma_instance_service.dart';
+import 'package:fedi/pleroma/oauth/pleroma_oauth_last_launched_host_to_login_local_preference_bloc.dart';
+import 'package:fedi/pleroma/oauth/pleroma_oauth_service.dart';
+import 'package:firebase_analytics/firebase_analytics.dart';
 import 'package:firebase_analytics/observer.dart';
 import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'package:flutter/foundation.dart';
@@ -32,11 +33,13 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:logging/logging.dart';
 import 'package:moor/moor.dart';
+import 'package:overlay_support/overlay_support.dart';
 import 'package:pedantic/pedantic.dart';
+import 'package:provider/provider.dart';
 
 var _logger = Logger("main.dart");
 
-ICurrentAuthInstanceContextBloc currentInstanceContextBloc;
+FirebaseAnalytics analytics = FirebaseAnalytics();
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -45,8 +48,12 @@ void main() async {
   // This is only to be used for confirming that reports are being
   // submitted as expected. It is not intended to be used for everyday
   // development.
+
   // Pass all uncaught errors from the framework to Crashlytics.
   FlutterError.onError = Crashlytics.instance.recordFlutterError;
+
+
+  runApp(MaterialApp(home: SplashPage()));
 
   IInitBloc initBloc = InitBloc();
   unawaited(initBloc.performAsyncInit());
@@ -63,10 +70,7 @@ void main() async {
           .distinct(
               (previous, next) => previous?.userAtHost == next?.userAtHost)
           .listen((currentInstance) {
-        startApp(
-          appContextBloc: initBloc.appContextBloc,
-          currentInstance: currentInstance,
-        );
+        buildCurrentInstanceApp(initBloc.appContextBloc, currentInstance);
       });
     } else if (newState == AsyncInitLoadingState.failed) {
       _logger.severe(() => "failed to init App");
@@ -74,18 +78,59 @@ void main() async {
   });
 }
 
-var _splashPageApp = MaterialApp(home: SplashPage());
-void runSplashPageApp() {
-  runApp(_splashPageApp);
+void _handleLoginOnAndroidWithoutChrome(
+    AppContextBloc appContextBloc, Uri initialUri) async {
+  var pleromaOAuthLastLaunchedHostToLoginLocalPreferenceBloc = appContextBloc
+      .get<IPleromaOAuthLastLaunchedHostToLoginLocalPreferenceBloc>();
+
+  var lastLaunchedHost =
+      pleromaOAuthLastLaunchedHostToLoginLocalPreferenceBloc.value;
+
+  _logger.finest(() => "initialUri = $initialUri "
+      "lastLaunchedHost = $lastLaunchedHost");
+  if (lastLaunchedHost != null) {
+    var currentInstanceBloc = appContextBloc.get<ICurrentAuthInstanceBloc>();
+    var authHostBloc = AuthHostBloc(
+      instanceBaseUrl: Uri.parse(lastLaunchedHost),
+      preferencesService: appContextBloc.get(),
+      connectionService: appContextBloc.get(),
+      currentInstanceBloc: currentInstanceBloc,
+      pleromaOAuthLastLaunchedHostToLoginLocalPreferenceBloc:
+          pleromaOAuthLastLaunchedHostToLoginLocalPreferenceBloc,
+      // doesn't matter here
+      isPleromaInstance: false,
+    );
+    await authHostBloc.performAsyncInit();
+    String authCode = IPleromaOAuthService.extractAuthCodeFromUri(initialUri);
+
+    try {
+      var authInstance = await authHostBloc.loginWithAuthCode(authCode);
+
+      if (authInstance != null) {
+        await currentInstanceBloc.changeCurrentInstance(authInstance);
+      }
+    } catch (e, stackTrace) {
+      _logger.warning(
+          () => "Failed to _handleLoginOnAndroidWithoutChrome ", e, stackTrace);
+    }
+  }
 }
 
-void startApp({
-  @required AppContextBloc appContextBloc,
-  @required AuthInstance currentInstance,
-}) async {
-  _logger.finest(() => "buildCurrentInstanceApp $startApp");
+CurrentAuthInstanceContextBloc currentInstanceContextBloc;
+
+void showSplashPage(AppContextBloc appContextBloc) {
+  var easyLocalization = _buildEasyLocalization(
+      child: MyApp(
+          child: Provider<IAppContextBloc>.value(
+              value: appContextBloc, child: (const SplashPage()))));
+  runApp(easyLocalization);
+}
+
+void buildCurrentInstanceApp(
+    AppContextBloc appContextBloc, AuthInstance currentInstance) async {
+  _logger.finest(() => "buildCurrentInstanceApp $buildCurrentInstanceApp");
   if (currentInstance != null) {
-    runSplashPageApp();
+    showSplashPage(appContextBloc);
     currentInstanceContextBloc?.dispose();
 
     currentInstanceContextBloc = CurrentAuthInstanceContextBloc(
@@ -99,84 +144,62 @@ void startApp({
     await currentInstanceContextBloc.performAsyncInit();
     _logger.finest(
         () => "buildCurrentInstanceApp CurrentInstanceContextLoadingPage");
-
-    startUserLoggedApp(appContextBloc, currentInstanceContextBloc);
+    runApp(_buildEasyLocalization(
+        child: appContextBloc.provideContextToChild(
+            child: currentInstanceContextBloc.provideContextToChild(
+                child:
+                    DisposableProvider<ICurrentAuthInstanceContextLoadingBloc>(
+                        create: (context) {
+                          var currentAuthInstanceContextLoadingBloc = CurrentAuthInstanceContextLoadingBloc(
+                              myAccountBloc:
+                                  IMyAccountBloc.of(context, listen: false),
+                              pleromaInstanceService:
+                                  IPleromaInstanceService.of(context,
+                                      listen: false),
+                              currentAuthInstanceBloc:
+                                  ICurrentAuthInstanceBloc.of(context,
+                                      listen: false),
+                            );
+                          currentAuthInstanceContextLoadingBloc
+                              .performAsyncInit();
+                          return currentAuthInstanceContextLoadingBloc;
+                        },
+                        child: MyApp(
+                            child: CurrentAuthInstanceContextLoadingWidget(
+                          child: DisposableProvider<IHomeBloc>(
+                              create: (context) =>
+                                  HomeBloc(startTab: HomeTab.timelines),
+                              child: const HomePage()),
+                        )))))));
   } else {
-    startUserNotLoggedApp(appContextBloc);
+    runApp(_buildEasyLocalization(
+        child: appContextBloc.provideContextToChild(
+            child: DisposableProvider<IJoinAuthInstanceBloc>(
+                create: (context) => JoinAuthInstanceBloc(),
+                child:
+                    const MyApp(child: FromScratchJoinAuthInstancePage())))));
   }
 }
 
-void startUserLoggedApp(AppContextBloc appContextBloc,
-    ICurrentAuthInstanceContextBloc currentInstanceContextBloc) {
-  runApp(
-    appContextBloc.provideContextToChild(
-      child: currentInstanceContextBloc.provideContextToChild(
-        child: DisposableProvider<ICurrentAuthInstanceContextLoadingBloc>(
-          create: (context) {
-            var authInstanceContextLoadingBloc =
-                CurrentAuthInstanceContextLoadingBloc(
-              myAccountBloc: IMyAccountBloc.of(context, listen: false),
-              pleromaInstanceService:
-                  IPleromaInstanceService.of(context, listen: false),
-              currentAuthInstanceBloc:
-                  ICurrentAuthInstanceBloc.of(context, listen: false),
-            );
-            authInstanceContextLoadingBloc.performAsyncInit();
-            return authInstanceContextLoadingBloc;
-          },
-          child: buildEasyLocalization(
-            child: FediApp(
-              home: CurrentAuthInstanceContextLoadingWidget(
-                child: DisposableProvider<IHomeBloc>(
-                    create: (context) => HomeBloc(startTab: HomeTab.timelines),
-                    child: const HomePage()),
-              ),
-            ),
-          ),
-        ),
-      ),
-    ),
-  );
-}
-
-void startUserNotLoggedApp(AppContextBloc appContextBloc) {
-  runApp(
-    appContextBloc.provideContextToChild(
-      child: DisposableProvider<IJoinAuthInstanceBloc>(
-        create: (context) => JoinAuthInstanceBloc(),
-        child: buildEasyLocalization(
-          child: const FediApp(
-            home: FromScratchJoinAuthInstancePage(),
-          ),
-        ),
-      ),
-    ),
-  );
-}
-
-Widget buildEasyLocalization({Widget child}) {
+Widget _buildEasyLocalization({@required Widget child}) {
   return EasyLocalization(
-      key: PageStorageKey("EasyLocalization"),
-      supportedLocales: [Locale('en', 'US')],
-      path: "assets/langs",
-      assetLoader: CodegenLoader(),
-      preloaderColor: FediColors.primaryColorDark,
-      preloaderWidget: _splashPageApp,
-      child: child);
+    key: PageStorageKey("EasyLocalization"),
+    supportedLocales: [Locale('en', 'US')],
+    path: "assets/langs",
+    child: child,
+  );
 }
 
-class FediApp extends StatelessWidget {
-  final Widget home;
+class MyApp extends StatelessWidget {
+  final Widget child;
 
-  const FediApp({@required this.home});
+  const MyApp({@required this.child});
 
   @override
   Widget build(BuildContext context) {
     SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
 
-    var analyticsService = IAnalyticsService.of(context, listen: false);
-
-    return MaterialApp(
+    var app = MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Fedi2',
       localizationsDelegates: context.localizationDelegates,
@@ -184,11 +207,29 @@ class FediApp extends StatelessWidget {
       locale: context.locale,
       theme: fediTheme,
       initialRoute: "/",
-      home: home,
+      home: child,
       navigatorObservers: [
-        FirebaseAnalyticsObserver(
-            analytics: analyticsService.firebaseAnalytics),
+        FirebaseAnalyticsObserver(analytics: analytics),
       ],
     );
+    return OverlaySupport(child: app);
+  }
+}
+
+void initLog() {
+  if (kReleaseMode) {
+    Logger.root.level = Level.OFF; // defaults to Level.INFO
+  } else {
+    Logger.root.level = Level.ALL; // defaults to Level.INFO
+    Logger.root.onRecord.listen((record) {
+      print('${record.level.name}(${record.loggerName}): ${record.time}: '
+          '${record.message}');
+      if (record.error != null) {
+        print("\n${record.error}");
+      }
+      if (record.stackTrace != null) {
+        print("\n${record.stackTrace}");
+      }
+    });
   }
 }
