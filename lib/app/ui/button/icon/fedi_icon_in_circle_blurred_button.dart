5c2f0264c992e4d408b8a100ea2871aa083b4212
Yevhenii Zapletin <zapletin.yevhenii@gmail.com>
2020-07-19 14:26:59 +0300

    fix blurred buttons

--- lib/app/ui/button/icon/fedi_icon_in_circle_blurred_button.dart
+++ lib/app/ui/button/icon/fedi_icon_in_circle_blurred_button.dart
@@ -26,21 +26,23 @@ class FediIconInCircleBlurredButton extends StatelessWidget {
   Widget build(BuildContext context) => Container(
       width: size + borderWidth * 2,
       height: size + borderWidth * 2,
-      decoration: BoxDecoration(
-        shape: BoxShape.circle,
-        color: FediColors.darkGrey.withOpacity(0.3),
-        border: Border.all(
-          color: FediColors.white,
-          width: borderWidth,
-        ),
-      ),
       child: ClipRRect(
         borderRadius: BorderRadius.circular(size),
         child: BackdropFilter(
-          child: FediIconButton(
-            padding: EdgeInsets.zero,
-            icon: Icon(iconData, size: iconSize, color: FediColors.white),
-            onPressed: onPressed,
+          child: Container(
+            decoration: BoxDecoration(
+              shape: BoxShape.circle,
+              color: FediColors.darkGrey.withOpacity(0.3),
+              border: Border.all(
+                color: FediColors.white,
+                width: borderWidth,
+              ),
+            ),
+            child: FediIconButton(
+              padding: EdgeInsets.zero,
+              icon: Icon(iconData, size: iconSize, color: FediColors.white),
+              onPressed: onPressed,
+            ),
           ),
           filter: ImageFilter.blur(sigmaX: 3.0, sigmaY: 3.0),
         ),
--- lib/app/ui/button/text/fedi_blurred_text_button.dart
+++ lib/app/ui/button/text/fedi_blurred_text_button.dart
@@ -31,35 +31,36 @@ class FediBlurredTextButton extends StatelessWidget {
     var borderRadius = BorderRadius.all(Radius.circular(calculatedHeight / 2));
     return InkWell(
       onTap: onPressed,
-      child: Container(
-          height: calculatedHeight,
-          decoration: BoxDecoration(
-            shape: BoxShape.rectangle,
-            color: FediColors.darkGrey.withOpacity(0.3),
-            borderRadius: borderRadius,
-            border: Border.all(
-              color: FediColors.white,
-              width: borderWidth,
+      child: ClipRRect(
+        borderRadius: borderRadius,
+        child: BackdropFilter(
+          child: Container(
+            height: calculatedHeight,
+            decoration: BoxDecoration(
+              shape: BoxShape.rectangle,
+              color: FediColors.darkGrey.withOpacity(0.3),
+              borderRadius: borderRadius,
+              border: Border.all(
+                color: FediColors.white,
+                width: borderWidth,
+              ),
             ),
-          ),
-          child: ClipRRect(
-            borderRadius: borderRadius,
-            child: BackdropFilter(
-              child: Center(
-                child: Padding(
-                  padding: FediPadding.buttonHorizontalPadding,
-                  child: Text(
-                    text,
-                    textAlign: TextAlign.center,
-                    style: textStyle.copyWith(
-                      color: textStyle.color.withOpacity(0.8),
-                    ),
+            child: Center(
+              child: Padding(
+                padding: FediPadding.buttonHorizontalPadding,
+                child: Text(
+                  text,
+                  textAlign: TextAlign.center,
+                  style: textStyle.copyWith(
+                    color: textStyle.color.withOpacity(0.8),
                   ),
                 ),
               ),
-              filter: ImageFilter.blur(sigmaX: 3.0, sigmaY: 3.0),
             ),
-          )),
+          ),
+          filter: ImageFilter.blur(sigmaX: 3.0, sigmaY: 3.0),
+        ),
+      ),
     );
   }
 }
