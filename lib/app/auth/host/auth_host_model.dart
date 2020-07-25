c3b61a6e511bc2df2c7c8f05e251e541ef86df0e
Yevhenii Zapletin <zapletin.yevhenii@gmail.com>
2020-07-25 16:55:49 +0300

    check is instance enable registrations

---
+++ lib/app/auth/host/auth_host_model.dart
@@ -0,0 +1,3 @@
+class RegistrationNotEnabledAuthHostException implements Exception {}
+class CantRetrieveAppTokenAuthHostException implements Exception {}
+class CantRegisterAppAuthHostException implements Exception {}
