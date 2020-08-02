2664fdb135abb24ba7280c4a223868a57f5a91af
Yevhenii Zapletin <zapletin.yevhenii@gmail.com>
2020-08-02 18:26:42 +0300

    improve dialogs UI

--- lib/app/ui/divider/fedi_ultra_light_grey_divider.dart
+++ lib/app/ui/divider/fedi_ultra_light_grey_divider.dart
@@ -2,8 +2,12 @@ import 'package:fedi/app/ui/fedi_colors.dart';
 import 'package:flutter/cupertino.dart';
 
 class FediUltraLightGreyDivider extends StatelessWidget {
+  final double height;
+
+
+  const FediUltraLightGreyDivider({this.height = 1});
+
   @override
   Widget build(BuildContext context) =>
-      Container(height: 1, width: double.infinity, color: FediColors.ultraLightGrey);
-  const FediUltraLightGreyDivider();
+      Container(height: height, width: double.infinity, color: FediColors.ultraLightGrey);
 }
