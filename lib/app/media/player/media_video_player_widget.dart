d3140d3563b8e719729f273897946cc970477a75
Yevhenii Zapletin <zapletin.yevhenii@gmail.com>
2020-08-15 20:00:04 +0300

    fix video player for audio-only mp4 files

--- lib/app/media/player/media_video_player_widget.dart
+++ lib/app/media/player/media_video_player_widget.dart
@@ -3,7 +3,10 @@ import 'dart:io';
 import 'package:chewie/chewie.dart';
 import 'package:fedi/app/media/player/media_player_widget.dart';
 import 'package:flutter/widgets.dart';
+import 'package:logging/logging.dart';
 import 'package:video_player/video_player.dart';
+
+var _logger = Logger("media_video_player_widget.dart");
 
 class MediaVideoPlayerWidget extends MediaPlayerWidget<ChewieController> {
   MediaVideoPlayerWidget.localFile({@required File localFile})
@@ -16,8 +19,14 @@ class MediaVideoPlayerWidget extends MediaPlayerWidget<ChewieController> {
   ChewieController createDisplayController(
       VideoPlayerController videoPlayerController) {
     videoPlayerController.setVolume(0);
+    var aspectRatio = videoPlayerController.value.aspectRatio;
+    _logger.finest(() => "createDisplayController aspectRatio ${aspectRatio}");
+
+    if (aspectRatio.isNaN || aspectRatio.isInfinite) {
+      aspectRatio = null;
+    }
     return ChewieController(
-        aspectRatio: videoPlayerController.value.aspectRatio,
+        aspectRatio: aspectRatio,
         videoPlayerController: videoPlayerController,
         autoInitialize: false,
         allowFullScreen: false,
