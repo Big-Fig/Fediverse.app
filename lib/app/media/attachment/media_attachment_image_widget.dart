41f8cc291351b92e2d8b5517cd84a69d5c439cae
Yevhenii Zapletin <zapletin.yevhenii@gmail.com>
2020-08-15 17:19:53 +0300

    better UI for white-transparent images

--- lib/app/media/attachment/media_attachment_image_widget.dart
+++ lib/app/media/attachment/media_attachment_image_widget.dart
@@ -1,5 +1,6 @@
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:fedi/app/media/attachment/preview/media_attachment_preview_page.dart';
+import 'package:fedi/app/ui/fedi_colors.dart';
 import 'package:fedi/app/ui/fedi_padding.dart';
 import 'package:fedi/app/ui/progress/fedi_circular_progress_indicator.dart';
 import 'package:fedi/pleroma/media/attachment/pleroma_media_attachment_model.dart';
@@ -19,36 +20,39 @@ class MediaAttachmentImageWidget extends StatelessWidget {
           goToMediaAttachmentPreviewPage(context,
               mediaAttachment: mediaAttachment);
         },
-        child: CachedNetworkImage(
-          imageUrl: mediaAttachment.previewUrl,
-          fit: BoxFit.cover,
-          placeholder: (context, url) => Padding(
-            padding: FediPadding.allBigPadding,
-            child: Center(
-              child: Container(
-                width: 30,
-                height: 30,
-                child: FediCircularProgressIndicator(),
+        child: Container(
+          color: FediColors.ultraLightGrey,
+          child: CachedNetworkImage(
+            imageUrl: mediaAttachment.previewUrl,
+            fit: BoxFit.cover,
+            placeholder: (context, url) => Padding(
+              padding: FediPadding.allBigPadding,
+              child: Center(
+                child: Container(
+                  width: 30,
+                  height: 30,
+                  child: FediCircularProgressIndicator(),
+                ),
               ),
             ),
-          ),
-          imageBuilder: (context, imageProvider) {
-            if (maxHeight != null) {
-              return LimitedBox(
-                maxHeight: maxHeight,
-                child: Image(
+            imageBuilder: (context, imageProvider) {
+              if (maxHeight != null) {
+                return LimitedBox(
+                  maxHeight: maxHeight,
+                  child: Image(
+                    fit: BoxFit.cover,
+                    image: imageProvider,
+                  ),
+                );
+              } else {
+                return Image(
                   fit: BoxFit.cover,
                   image: imageProvider,
-                ),
-              );
-            } else {
-              return Image(
-                fit: BoxFit.cover,
-                image: imageProvider,
-              );
-            }
-          },
-          errorWidget: (context, url, error) => Icon(Icons.error),
+                );
+              }
+            },
+            errorWidget: (context, url, error) => Icon(Icons.error),
+          ),
         ));
   }
 }
