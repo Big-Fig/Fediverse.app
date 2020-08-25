959756c8c6b2067bb335304c8aa7b8e35e36b378
Yevhenii Zapletin <zapletin.yevhenii@gmail.com>
2020-08-25 14:51:21 +0300

    open hashtag url from hashtag page

--- lib/app/hashtag/hashtag_page.dart
+++ lib/app/hashtag/hashtag_page.dart
@@ -2,6 +2,7 @@ import 'package:fedi/app/auth/instance/current/current_auth_instance_bloc.dart';
 import 'package:fedi/app/hashtag/hashtag_model.dart';
 import 'package:fedi/app/hashtag/status/list/hashtag_timeline_status_cached_list_bloc_impl.dart';
 import 'package:fedi/app/list/cached/pleroma_cached_list_bloc.dart';
+import 'package:fedi/app/status/content/statuc_content_link_helper.dart';
 import 'package:fedi/app/status/list/cached/status_cached_list_bloc.dart';
 import 'package:fedi/app/status/pagination/cached/status_cached_pagination_bloc_impl.dart';
 import 'package:fedi/app/status/pagination/list/status_cached_pagination_list_timeline_widget.dart';
@@ -9,7 +10,10 @@ import 'package:fedi/app/status/pagination/list/status_cached_pagination_list_wi
 import 'package:fedi/app/status/repository/status_repository.dart';
 import 'package:fedi/app/status/status_model.dart';
 import 'package:fedi/app/timeline/settings/local_preferences/timeline_settings_local_preferences_bloc.dart';
+import 'package:fedi/app/ui/button/icon/fedi_icon_button.dart';
+import 'package:fedi/app/ui/fedi_icons.dart';
 import 'package:fedi/app/ui/page/fedi_sub_page_title_app_bar.dart';
+import 'package:fedi/app/url/url_helper.dart';
 import 'package:fedi/collapsible/collapsible_owner_widget.dart';
 import 'package:fedi/disposable/disposable_provider.dart';
 import 'package:fedi/pleroma/timeline/pleroma_timeline_service.dart';
@@ -30,6 +34,9 @@ class HashtagPage extends StatelessWidget {
       appBar: FediSubPageTitleAppBar(
         centerTitle: false,
         title: "#${hashtag.name}",
+        actions: <Widget>[
+          buildOpenInBrowserAction(context),
+        ],
       ),
       body: SafeArea(
         child: CollapsibleOwnerWidget(
@@ -39,6 +46,15 @@ class HashtagPage extends StatelessWidget {
         ),
       ),
     );
+  }
+
+  FediIconButton buildOpenInBrowserAction(BuildContext context) {
+    return FediIconButton(
+          icon: Icon(FediIcons.browser),
+          onPressed: () {
+            UrlHelper.handleUrlClick(context, hashtag.url);
+          },
+        );
   }
 }
 
--- lib/app/url/url_helper.dart
+++ lib/app/url/url_helper.dart
@@ -1,20 +1,31 @@
 import 'package:easy_localization/easy_localization.dart';
+import 'package:fedi/app/auth/instance/current/current_auth_instance_bloc.dart';
 import 'package:fedi/app/ui/dialog/alert/fedi_simple_alert_dialog.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:url_launcher/url_launcher.dart';
 
 class UrlHelper {
   static Future handleUrlClick(BuildContext context, String url) async {
+    var currentAuthInstanceBloc =
+        ICurrentAuthInstanceBloc.of(context, listen: false);
+
+    var uri = Uri.parse(url);
+
+    var host = uri.host;
+    if (host?.isNotEmpty != true) {
+      var urlHost = currentAuthInstanceBloc.currentInstance.urlHost;
+      var urlSchema = currentAuthInstanceBloc.currentInstance.urlSchema;
+
+      url = "${urlSchema}://$urlHost$url";
+    }
+
     if (await canLaunch(url)) {
       await launch(url);
     } else {
-
       await FediSimpleAlertDialog(
-              context: context,
-              title: tr("link.error.dialog.title"),
-              content: tr("link.error.dialog.content",
-                  args: [url]))
-          .show(context);
+          context: context,
+          title: tr("link.error.dialog.title"),
+          content: tr("link.error.dialog.content", args: [url])).show(context);
     }
   }
 
