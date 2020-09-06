0faf22b9065e34060cde1039cf395b31f3095960
Yevhenii Zapletin <zapletin.yevhenii@gmail.com>
2020-09-07 00:05:42 +0300

    fix custom list multi account selection

--- lib/app/account/select/multi/multi_select_account_bloc_impl.dart
+++ lib/app/account/select/multi/multi_select_account_bloc_impl.dart
@@ -3,7 +3,10 @@ import 'package:fedi/app/account/select/multi/multi_select_account_bloc.dart';
 import 'package:fedi/disposable/disposable_owner.dart';
 import 'package:fedi/disposable/disposable_provider.dart';
 import 'package:flutter/widgets.dart';
+import 'package:logging/logging.dart';
 import 'package:rxdart/rxdart.dart';
+
+var _logger = Logger("multi_select_account_page.dart");
 
 class MultiSelectAccountBloc extends DisposableOwner
     implements IMultiSelectAccountBloc {
@@ -66,14 +69,22 @@ class MultiSelectAccountBloc extends DisposableOwner
   @override
   void addAccountSelection(IAccount account) {
     selectedAccounts.add(account);
+
+    _logger.finest(() => "addAccountSelection $account");
+    onSelectionChanged();
+  }
+
+  void onSelectionChanged() {
     selectedAccountsSubject.add(selectedAccounts);
+    _logger.finest(() =>
+        "onSelectionChanged ${selectedAccounts.length} $selectedAccounts");
   }
 
   @override
   void removeAccountSelection(IAccount account) {
-    selectedAccounts.removeWhere((currentAccount) => account.remoteId ==
-        account.remoteId);
-    selectedAccountsSubject.add(selectedAccounts);
+    _logger.finest(() => "removeAccountSelection $account");
+    selectedAccounts
+        .removeWhere((currentAccount) => account.remoteId == account.remoteId);
+    onSelectionChanged();
   }
-
 }
