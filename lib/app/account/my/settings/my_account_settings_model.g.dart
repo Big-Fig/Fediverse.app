8e688bf6c2d0c7c6b940892ca34763d8d953a5f4
Yevhenii Zapletin <zapletin.yevhenii@gmail.com>
2020-09-12 12:29:55 +0300

    fix more fields to account settings: generated code

--- lib/app/account/my/settings/my_account_settings_model.g.dart
+++ lib/app/account/my/settings/my_account_settings_model.g.dart
@@ -19,6 +19,24 @@ class MyAccountSettingsAdapter extends TypeAdapter<MyAccountSettings> {
         case 1:
           obj.isNewChatsEnabled = reader.read() as bool;
           break;
+        case 2:
+          obj.isAlwaysShowSpoiler = reader.read() as bool;
+          break;
+        case 3:
+          obj.isAlwaysShowNsfw = reader.read() as bool;
+          break;
+        case 4:
+          obj.defaultVisibility = reader.read() as PleromaVisibility;
+          break;
+        case 5:
+          obj.markMediaNsfwByDefault = reader.read() as bool;
+          break;
+        case 6:
+          obj.foregroundSoundForChatAndDm = reader.read() as bool;
+          break;
+        case 7:
+          obj.foregroundSoundForMention = reader.read() as bool;
+          break;
       }
     }
     return obj;
@@ -26,10 +44,22 @@ class MyAccountSettingsAdapter extends TypeAdapter<MyAccountSettings> {
 
   @override
   void write(BinaryWriter writer, MyAccountSettings obj) {
-    writer.writeByte(2);
+    writer.writeByte(8);
     writer.writeByte(0);
     writer.write(obj.isRealtimeWebSocketsEnabled);
     writer.writeByte(1);
     writer.write(obj.isNewChatsEnabled);
+    writer.writeByte(2);
+    writer.write(obj.isAlwaysShowSpoiler);
+    writer.writeByte(3);
+    writer.write(obj.isAlwaysShowNsfw);
+    writer.writeByte(4);
+    writer.write(obj.defaultVisibility);
+    writer.writeByte(5);
+    writer.write(obj.markMediaNsfwByDefault);
+    writer.writeByte(6);
+    writer.write(obj.foregroundSoundForChatAndDm);
+    writer.writeByte(7);
+    writer.write(obj.foregroundSoundForMention);
   }
 }
