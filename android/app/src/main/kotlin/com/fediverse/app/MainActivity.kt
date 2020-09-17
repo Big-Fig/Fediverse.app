f7a6df26f2a228f178b22a05b317688bdf2880dd
Yevhenii Zapletin <zapletin.yevhenii@gmail.com>
2020-09-17 23:41:00 +0300

    use v2 flutter embedding

--- android/app/src/main/kotlin/com/fediverse/app/MainActivity.kt
+++ android/app/src/main/kotlin/com/fediverse/app/MainActivity.kt
@@ -5,16 +5,14 @@ import android.app.NotificationManager
 import android.content.Context
 import android.os.Build
 import android.os.Bundle
-import io.flutter.app.FlutterActivity
-import io.flutter.plugins.GeneratedPluginRegistrant
+import io.flutter.embedding.android.FlutterActivity;
+import io.flutter.embedding.engine.FlutterEngine;
 
 class MainActivity : FlutterActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         registerNotificationChannels()
-        GeneratedPluginRegistrant.registerWith(this)
     }
-
 
     override fun onResume() {
         super.onResume()
