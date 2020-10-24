bacd15b490416802d21235e95effc61115fbde2c
Yevhenii Zapletin <zapletin.yevhenii@gmail.com>
2020-10-24 21:35:44 +0300

    post status action collapsed mode: text post action to icon in circle button. Add multiline mode

--- lib/app/status/post/action/post_status_post_icon_in_circle_action_widget.dart
+++ lib/app/status/post/action/post_status_post_icon_in_circle_action_widget.dart
@@ -1,13 +1,13 @@
 import 'package:fedi/app/async/pleroma_async_operation_button_builder_widget.dart';
 import 'package:fedi/app/status/post/action/post_status_post_overlay_notification.dart';
 import 'package:fedi/app/status/post/post_status_bloc.dart';
-import 'package:fedi/app/ui/button/icon/fedi_icon_button.dart';
+import 'package:fedi/app/ui/button/icon/fedi_icon_in_circle_filled_button.dart';
 import 'package:fedi/app/ui/fedi_icons.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-class PostStatusPostIconActionWidget extends StatelessWidget {
-  const PostStatusPostIconActionWidget();
+class PostStatusPostIconInCircleActionWidget extends StatelessWidget {
+  const PostStatusPostIconInCircleActionWidget();
 
   @override
   Widget build(BuildContext context) {
@@ -33,8 +33,8 @@ class PostStatusPostIconActionWidget extends StatelessWidget {
               }
             },
             builder: (BuildContext context, onPressed) {
-              return FediIconButton(
-                icon: Icon(FediIcons.send),
+              return FediIconInCircleFilledButton(
+                FediIcons.send,
                 onPressed: isReadyToPost ? onPressed : null,
               );
             },
