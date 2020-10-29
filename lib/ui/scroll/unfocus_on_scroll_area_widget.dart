bc4d511a9f8d60346052267ec77a722d9a3b9f00
Yevhenii Zapletin <zapletin.yevhenii@gmail.com>
2020-10-29 19:52:02 +0300

    rework new post status page widgets building

--- lib/ui/scroll/unfocus_on_scroll_area_widget.dart
+++ lib/ui/scroll/unfocus_on_scroll_area_widget.dart
@@ -3,15 +3,13 @@ import 'package:flutter/cupertino.dart';
 class UnfocusOnScrollAreaWidget extends StatelessWidget {
   final Widget child;
 
-
-  UnfocusOnScrollAreaWidget({@required this.child});
+  const UnfocusOnScrollAreaWidget({@required this.child});
 
   @override
   Widget build(BuildContext context) => GestureDetector(
-        behavior: HitTestBehavior.opaque,
-        onPanDown: (_) {
-          FocusScope.of(context).requestFocus(FocusNode());
-        },
-        child: child);
-
+      behavior: HitTestBehavior.opaque,
+      onPanDown: (_) {
+        FocusScope.of(context).requestFocus(FocusNode());
+      },
+      child: child);
 }
