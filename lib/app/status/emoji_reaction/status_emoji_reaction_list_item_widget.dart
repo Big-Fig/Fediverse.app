c3a613f594f63863053e73f496a74c418fdc9613
Yevhenii Zapletin <zapletin.yevhenii@gmail.com>
2020-10-31 18:01:59 +0300

    rework status emoji reaction building

--- lib/app/status/emoji_reaction/status_emoji_reaction_list_item_widget.dart
+++ lib/app/status/emoji_reaction/status_emoji_reaction_list_item_widget.dart
@@ -6,72 +6,117 @@ import 'package:fedi/app/ui/theme/fedi_ui_theme_model.dart';
 import 'package:fedi/pleroma/status/pleroma_status_model.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class StatusEmojiReactionListItemWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    var statusEmojiReactionBloc =
-        IStatusEmojiReactionBloc.of(context, listen: true);
+    var statusEmojiReactionBloc = IStatusEmojiReactionBloc.of(context);
     return StreamBuilder<IPleromaStatusEmojiReaction>(
         stream: statusEmojiReactionBloc.emojiReactionStream,
-        initialData: statusEmojiReactionBloc.emojiReaction,
         builder: (context, snapshot) {
           var emojiReaction = snapshot.data;
 
           if (emojiReaction == null) {
             // may occurs when user delete own reaction when new count is 0
-            return SizedBox.shrink();
+            return const SizedBox.shrink();
           }
 
-          return PleromaAsyncOperationButtonBuilderWidget(
-            showProgressDialog: false,
-            asyncButtonAction: () =>
-                statusEmojiReactionBloc.toggleEmojiReaction(),
-            builder: (BuildContext context, void Function() onPressed) {
-              var color = emojiReaction.me
-                  ? IFediUiColorTheme.of(context).primary
-                  : IFediUiColorTheme.of(context).lightGrey;
-              return Padding(
-                padding: const EdgeInsets.symmetric(
-                    horizontal: FediSizes.smallPadding),
-                child: InkWell(
-                  onTap: onPressed,
-                  child: Container(
-                    height: 36.0,
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(40.0),
-                      border: Border.all(
-                        color: color,
-                        width: 1.5,
-                      ),
-                    ),
-                    child: Padding(
-                      padding: const EdgeInsets.symmetric(
-                          horizontal: FediSizes.mediumPadding),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        crossAxisAlignment: CrossAxisAlignment.center,
-                        mainAxisSize: MainAxisSize.min,
-                        children: <Widget>[
-                          Text(
-                            emojiReaction.name,
-                            style: TextStyle(fontSize: 16, height: 14 / 16),
-                          ),
-                          const FediSmallHorizontalSpacer(),
-                          Text(
-                            "${emojiReaction.count}",
-                            style: IFediUiTextTheme.of(context)
-                                .mediumShortDarkGrey
-                                .copyWith(color: color),
-                          ),
-                        ],
-                      ),
-                    ),
-                  ),
-                ),
-              );
-            },
+          return Provider.value(
+            value: emojiReaction,
+            child: const _StatusEmojiReactionListItemBodyWidget(),
           );
         });
+  }
+
+  const StatusEmojiReactionListItemWidget();
+}
+
+class _StatusEmojiReactionListItemBodyWidget extends StatelessWidget {
+  const _StatusEmojiReactionListItemBodyWidget({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    var statusEmojiReactionBloc = IStatusEmojiReactionBloc.of(context);
+    var emojiReaction = Provider.of<IPleromaStatusEmojiReaction>(context);
+    return PleromaAsyncOperationButtonBuilderWidget(
+      showProgressDialog: false,
+      asyncButtonAction: () => statusEmojiReactionBloc.toggleEmojiReaction(),
+      builder: (BuildContext context, void Function() onPressed) {
+        var color = emojiReaction.me
+            ? IFediUiColorTheme.of(context).primary
+            : IFediUiColorTheme.of(context).lightGrey;
+        return Padding(
+          padding:
+              const EdgeInsets.symmetric(horizontal: FediSizes.smallPadding),
+          child: InkWell(
+            onTap: onPressed,
+            child: Container(
+              height: 36.0,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(40.0),
+                border: Border.all(
+                  color: color,
+                  width: 1.5,
+                ),
+              ),
+              child: Padding(
+                padding: const EdgeInsets.symmetric(
+                    horizontal: FediSizes.mediumPadding),
+                child: const _StatusEmojiReactionListItemContentWidget(),
+              ),
+            ),
+          ),
+        );
+      },
+    );
+  }
+}
+
+class _StatusEmojiReactionListItemContentWidget extends StatelessWidget {
+  const _StatusEmojiReactionListItemContentWidget({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    var emojiReaction = Provider.of<IPleromaStatusEmojiReaction>(context);
+    var color = emojiReaction.me
+        ? IFediUiColorTheme.of(context).primary
+        : IFediUiColorTheme.of(context).lightGrey;
+    return Container(
+      height: 36.0,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(40.0),
+        border: Border.all(
+          color: color,
+          width: 1.5,
+        ),
+      ),
+      child: Padding(
+        padding:
+            const EdgeInsets.symmetric(horizontal: FediSizes.mediumPadding),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisSize: MainAxisSize.min,
+          children: <Widget>[
+            Text(
+              emojiReaction.name,
+              style: TextStyle(fontSize: 16, height: 14 / 16),
+            ),
+            const FediSmallHorizontalSpacer(),
+            Text(
+              "${emojiReaction.count}",
+              style: IFediUiTextTheme.of(context)
+                  .mediumShortDarkGrey
+                  .copyWith(color: color),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
