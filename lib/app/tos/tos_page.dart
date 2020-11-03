4952d485ed6103cffde6426f169c8adfc28dd158
Yevhenii Zapletin <zapletin.yevhenii@gmail.com>
2020-11-03 18:32:07 +0300

    improve tos page

--- lib/app/tos/tos_page.dart
+++ lib/app/tos/tos_page.dart
@@ -1,7 +1,7 @@
-import 'package:fedi/generated/l10n.dart';
 import 'package:fedi/app/ui/fedi_padding.dart';
-import 'package:fedi/app/ui/theme/fedi_ui_theme_model.dart';
 import 'package:fedi/app/ui/page/fedi_sub_page_title_app_bar.dart';
+import 'package:fedi/app/ui/theme/fedi_ui_theme_model.dart';
+import 'package:fedi/generated/l10n.dart';
 import 'package:flutter/material.dart';
 
 class TosPage extends StatelessWidget {
@@ -12,22 +12,35 @@ class TosPage extends StatelessWidget {
       appBar: FediSubPageTitleAppBar(
         title: S.of(context).app_tos_title,
       ),
-      body: SafeArea(
-        child: Padding(
-          padding: FediPadding.allBigPadding,
-          child: SingleChildScrollView(
-            child: FutureBuilder(
-              // TODO: Should be localized
-              future: DefaultAssetBundle.of(context)
-                  .loadString("assets/terms/terms.rtf"),
-              builder: (context, snapshot) {
-                return Text(
-                  snapshot.data ?? '',
-                  style: IFediUiTextTheme.of(context).mediumShortDarkGrey,
-                  softWrap: true,
-                );
-              },
-            ),
+      body: _TosPageBodyWigdet(),
+    );
+  }
+
+  const TosPage();
+}
+
+class _TosPageBodyWigdet extends StatelessWidget {
+  const _TosPageBodyWigdet({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Padding(
+        padding: FediPadding.allBigPadding,
+        child: SingleChildScrollView(
+          child: FutureBuilder(
+            // TODO: Should be localized
+            future: DefaultAssetBundle.of(context)
+                .loadString("assets/terms/terms.rtf"),
+            builder: (context, snapshot) {
+              return Text(
+                snapshot.data ?? '',
+                style: IFediUiTextTheme.of(context).mediumShortDarkGrey,
+                softWrap: true,
+              );
+            },
           ),
         ),
       ),
@@ -38,6 +51,6 @@ class TosPage extends StatelessWidget {
 void goToTosPage(BuildContext context) {
   Navigator.push(
     context,
-    MaterialPageRoute(builder: (context) => TosPage()),
+    MaterialPageRoute(builder: (context) => const TosPage()),
   );
 }
