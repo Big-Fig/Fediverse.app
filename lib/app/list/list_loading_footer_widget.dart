ca0d19a3637ef35f38594c6cf2ca54bf9b3b6e16
Yevhenii Zapletin <zapletin.yevhenii@gmail.com>
2020-11-03 17:47:16 +0300

    list loading indicators rework

--- lib/app/list/list_loading_footer_widget.dart
+++ lib/app/list/list_loading_footer_widget.dart
@@ -1,45 +1,47 @@
-import 'package:fedi/generated/l10n.dart';
 import 'package:fedi/app/ui/progress/fedi_circular_progress_indicator.dart';
+import 'package:fedi/generated/l10n.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:pull_to_refresh/pull_to_refresh.dart';
 
 class ListLoadingFooterWidget extends StatelessWidget {
   @override
-  Widget build(BuildContext context) => CustomFooter(
-        builder: (BuildContext context, LoadStatus mode) {
-          Widget body;
-
-          switch (mode) {
-            case LoadStatus.idle:
-              body = Text("");
-              break;
-            case LoadStatus.canLoading:
-              body = Text(
-                S.of(context).app_list_loading_state_canLoadMore,
-              );
-              break;
-            case LoadStatus.loading:
-              body = FediCircularProgressIndicator();
-              break;
-            case LoadStatus.noMore:
-              body = Text(
-                S.of(context).app_list_loading_state_noMoreData,
-              );
-              break;
-            case LoadStatus.failed:
-              body = Text(
-                S.of(context).app_list_loading_state_failed,
-              );
-              break;
-          }
-
-          return Container(
-            height: 55.0,
-            child: Center(child: body),
-          );
-        },
+  Widget build(BuildContext context) => const CustomFooter(
+        builder: _builder,
       );
 
   const ListLoadingFooterWidget();
+}
+
+Widget _builder(BuildContext context, LoadStatus mode) {
+  Widget body;
+
+  switch (mode) {
+    case LoadStatus.idle:
+      body = Text("");
+      break;
+    case LoadStatus.canLoading:
+      body = Text(
+        S.of(context).app_list_loading_state_canLoadMore,
+      );
+      break;
+    case LoadStatus.loading:
+      body = const FediCircularProgressIndicator();
+      break;
+    case LoadStatus.noMore:
+      body = Text(
+        S.of(context).app_list_loading_state_noMoreData,
+      );
+      break;
+    case LoadStatus.failed:
+      body = Text(
+        S.of(context).app_list_loading_state_failed,
+      );
+      break;
+  }
+
+  return Container(
+    height: 55.0,
+    child: Center(child: body),
+  );
 }
