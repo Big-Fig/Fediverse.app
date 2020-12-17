c639fdacbd816ab761bcc22a59471f1bd4f66b57
Yevhenii Zapletin <zapletin.yevhenii@gmail.com>
2020-12-17 19:49:26 +0300

    add moor inspector support for debugging

--- lib/app/database/app_database_service_impl.dart
+++ lib/app/database/app_database_service_impl.dart
@@ -1,9 +1,12 @@
 import 'package:fedi/app/database/app_database.dart';
+import 'package:fedi/app/package_info/package_info_helper.dart';
 import 'package:fedi/async/loading/init/async_init_loading_bloc_impl.dart';
 import 'package:fedi/database/database_service.dart';
 import 'package:fedi/disposable/disposable.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/widgets.dart';
 import 'package:moor_flutter/moor_flutter.dart';
+import 'package:moor_inspector/moor_inspector.dart';
 import 'package:provider/provider.dart';
 
 class MoorDatabaseService extends AsyncInitLoadingBloc
@@ -13,6 +16,8 @@ class MoorDatabaseService extends AsyncInitLoadingBloc
   MoorDatabaseService({@required this.dbName});
 
   AppDatabase appDatabase;
+
+  MoorInspector inspector;
 
   @override
   Future internalAsyncInit() async {
@@ -28,13 +33,34 @@ class MoorDatabaseService extends AsyncInitLoadingBloc
     // todo: improve re-opening new database during account switch
     moorRuntimeOptions.dontWarnAboutMultipleDatabases = true;
 
-
-    appDatabase = AppDatabase(FlutterQueryExecutor.inDatabaseFolder(
-        path: '$dbName.sqlite', logStatements: false));
+    var path = '$dbName.sqlite';
+    appDatabase = AppDatabase(
+      FlutterQueryExecutor.inDatabaseFolder(
+        path: path,
+        logStatements: false,
+      ),
+    );
 
     addDisposable(disposable: CustomDisposable(() async {
       await appDatabase.close();
     }));
+
+    if (!kReleaseMode) {
+      var packageId = await FediPackageInfoHelper.getPackageId();
+
+      final moorInspectorBuilder = MoorInspectorBuilder()
+        ..bundleId = packageId
+        ..icon = 'flutter'
+        ..addDatabase(path, appDatabase);
+      inspector = moorInspectorBuilder.build();
+
+      //Start server and announcement server
+      await inspector.start();
+
+      addDisposable(custom: () async {
+        await inspector.stop();
+      });
+    }
   }
 
   @override
