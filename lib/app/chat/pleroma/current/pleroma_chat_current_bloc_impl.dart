5f8d37099895b2793c5f5628cfa4180c317ec4f1
Yevhenii Zapletin <zapletin.yevhenii@gmail.com>
2021-02-14 19:02:43 +0300

    fix opened chats logic: support switching from chat to chat

--- lib/app/chat/pleroma/current/pleroma_chat_current_bloc_impl.dart
+++ lib/app/chat/pleroma/current/pleroma_chat_current_bloc_impl.dart
@@ -1,9 +1,13 @@
-import 'package:fedi/app/chat/pleroma/pleroma_chat_model.dart';
 import 'package:fedi/app/chat/pleroma/current/pleroma_chat_current_bloc.dart';
+import 'package:fedi/app/chat/pleroma/pleroma_chat_model.dart';
 import 'package:fedi/disposable/disposable_owner.dart';
 import 'package:rxdart/rxdart.dart';
 
-class PleromaChatCurrentBloc extends DisposableOwner implements IPleromaChatCurrentBloc {
+// todo: refactor with similar conversation chat class
+class PleromaChatCurrentBloc extends DisposableOwner
+    implements IPleromaChatCurrentBloc {
+  final List<IPleromaChat> openedChats = [];
+
   BehaviorSubject<IPleromaChat> currentChatSubject = BehaviorSubject();
 
   PleromaChatCurrentBloc() {
@@ -18,14 +22,23 @@ class PleromaChatCurrentBloc extends DisposableOwner implements IPleromaChatCurr
 
   @override
   void onChatOpened(IPleromaChat chat) {
-    assert(currentChat == null);
     currentChatSubject.add(chat);
+    openedChats.add(chat);
   }
 
   @override
   void onChatClosed(IPleromaChat chat) {
-    assert(currentChat != null);
-    assert(currentChat.remoteId == chat.remoteId);
-    currentChatSubject.add(null);
+    var indexToRemove = openedChats.lastIndexWhere(
+      (currentChat) => currentChat.remoteId == chat.remoteId,
+    );
+
+    if (indexToRemove >= 0) {
+      openedChats.removeAt(indexToRemove);
+    }
+    if (openedChats?.isNotEmpty == true) {
+      currentChatSubject.add(null);
+    } else {
+      currentChatSubject.add(openedChats.last);
+    }
   }
 }
