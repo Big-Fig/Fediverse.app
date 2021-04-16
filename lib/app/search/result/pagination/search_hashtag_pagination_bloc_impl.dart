9570bca7e5ea95c7bf2af33432947bf9849c0824
Yevhenii Zapletin <zapletin.yevhenii@gmail.com>
2021-04-16 13:10:28 +0300

    fix search for accounts and hashtags

--- lib/app/search/result/pagination/search_hashtag_pagination_bloc_impl.dart
+++ lib/app/search/result/pagination/search_hashtag_pagination_bloc_impl.dart
@@ -1,3 +1,4 @@
+import 'package:collection/collection.dart';
 import 'package:fedi/app/hashtag/hashtag_model.dart';
 import 'package:fedi/app/search/result/pagination/search_adapter_pagination_bloc_impl.dart';
 import 'package:fedi/app/search/result/search_result_model.dart';
@@ -8,7 +9,7 @@ import 'package:flutter/widgets.dart';
 import 'package:provider/provider.dart';
 
 class SearchHashtagPaginationBloc
-    extends SearchAdapterPaginationBloc<IHashtag?> {
+    extends SearchAdapterPaginationBloc<IHashtag> {
   SearchHashtagPaginationBloc({
     required IPaginationBloc<PaginationPage<ISearchResultItem>,
             ISearchResultItem>
@@ -16,11 +17,13 @@ class SearchHashtagPaginationBloc
   }) : super(searchResultItemPaginationBloc: searchResultItemPaginationBloc);
 
   @override
-  PaginationPage<IHashtag?> mapPage(PaginationPage<ISearchResultItem> page) {
-    List<IHashtag?> items = page.items
+  PaginationPage<IHashtag> mapPage(PaginationPage<ISearchResultItem> page) {
+
+    List<IHashtag> items = page.items
         .where((searchResultItem) =>
             searchResultItem.type == SearchResultItemType.hashtag)
         .map((searchResultItem) => searchResultItem.hashtag)
+        .whereNotNull()
         .toList();
     return PaginationPage(
       requestedLimitPerPage: page.requestedLimitPerPage,
@@ -41,7 +44,7 @@ class SearchHashtagPaginationBloc
     required Widget child,
   }) {
     return DisposableProvider<
-        IPaginationBloc<PaginationPage<IHashtag?>, IHashtag?>>(
+        IPaginationBloc<PaginationPage<IHashtag>, IHashtag>>(
       create: (context) =>
           SearchHashtagPaginationBloc.createFromContext(context),
       child: child,
