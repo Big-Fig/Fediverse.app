0a723702c596181680486f0dbb1274362ecadef6
Yevhenii Zapletin <zapletin.yevhenii@gmail.com>
2021-04-24 22:57:30 +0300

    refactor json, local_preferences, hive tests to unify code between entities

--- test/analytics/app/local_preference/app_analytics_local_preferences_bloc_impl_test.dart
+++ test/analytics/app/local_preference/app_analytics_local_preferences_bloc_impl_test.dart
@@ -1,72 +1,23 @@
-import 'dart:async';
-
 import 'package:fedi/analytics/app/app_analytics_model.dart';
 import 'package:fedi/analytics/app/local_preferences/app_analytics_local_preference_bloc_impl.dart';
-import 'package:fedi/local_preferences/memory_local_preferences_service_impl.dart';
 import 'package:flutter_test/flutter_test.dart';
+
+import '../../../local_preferences/local_preferences_test_helper.dart';
+import '../app_analytics_model_test_helper.dart';
 
 // ignore_for_file: no-magic-number
 
 void main() {
-  late MemoryLocalPreferencesService memoryLocalPreferencesService;
-
-  setUp(() async {
-    memoryLocalPreferencesService = MemoryLocalPreferencesService();
-  });
-
-  tearDown(() {
-    memoryLocalPreferencesService.dispose();
-  });
-
   test('save & load', () async {
-    var appAnalyticsLocalPreferenceBloc = AppAnalyticsLocalPreferenceBloc(
-      memoryLocalPreferencesService,
+    await LocalPreferencesTestHelper.testSaveAndLoad<AppAnalyticsData,
+        AppAnalyticsLocalPreferenceBloc>(
+      defaultValue: AppAnalyticsLocalPreferenceBloc.defaultValue,
+      blocCreator: (localPreferencesService) =>
+          AppAnalyticsLocalPreferenceBloc(localPreferencesService),
+      testObjectCreator: ({required String seed}) =>
+          AppAnalyticsModelTestHelper.createTestAppAnalyticsData(
+        seed: seed,
+      ),
     );
-
-    await appAnalyticsLocalPreferenceBloc.performAsyncInit();
-
-    AppAnalyticsData? listenValue;
-    StreamSubscription streamSubscription =
-        appAnalyticsLocalPreferenceBloc.stream.listen((data) {
-      listenValue = data;
-    });
-
-    await Future.delayed(Duration(milliseconds: 100));
-
-    expect(appAnalyticsLocalPreferenceBloc.value,
-        AppAnalyticsLocalPreferenceBloc.defaultValue);
-    expect(listenValue, AppAnalyticsLocalPreferenceBloc.defaultValue);
-
-    var newValue = AppAnalyticsData(
-      appOpenedCount: 243,
-      isAppRated: true,
-    );
-
-    await appAnalyticsLocalPreferenceBloc.setValue(newValue);
-
-    await Future.delayed(Duration(milliseconds: 100));
-
-    expect(appAnalyticsLocalPreferenceBloc.value, newValue);
-    expect(listenValue, newValue);
-
-    appAnalyticsLocalPreferenceBloc = AppAnalyticsLocalPreferenceBloc(
-      memoryLocalPreferencesService,
-    );
-
-    await appAnalyticsLocalPreferenceBloc.performAsyncInit();
-
-    await streamSubscription.cancel();
-    listenValue = null;
-    streamSubscription = appAnalyticsLocalPreferenceBloc.stream.listen((data) {
-      listenValue = data;
-    });
-
-    await Future.delayed(Duration(milliseconds: 100));
-
-    expect(appAnalyticsLocalPreferenceBloc.value, newValue);
-    expect(listenValue, newValue);
-
-    await streamSubscription.cancel();
-    await appAnalyticsLocalPreferenceBloc.dispose();
   });
 }
