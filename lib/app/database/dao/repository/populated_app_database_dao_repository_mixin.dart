01916c2e9bb4524bb5bea4e2445dc653a114b45c
Yevhenii Zapletin <zapletin.yevhenii@gmail.com>
2021-05-12 16:12:47 +0300

    add findCount to populated_app_database_dao_repository_mixin.dart

--- lib/app/database/dao/repository/populated_app_database_dao_repository_mixin.dart
+++ lib/app/database/dao/repository/populated_app_database_dao_repository_mixin.dart
@@ -4,23 +4,23 @@ import 'package:fedi/repository/repository_model.dart';
 import 'package:moor/moor.dart';
 
 mixin PopulatedDatabaseDaoRepositoryMixin<
-        DbItem extends DataClass,
-        DbPopulatedItem,
-        AppItem,
-        DbId,
-        TableDsl extends Table,
-        TableInfoDsl extends TableInfo<TableDsl, DbItem>,
-        Filters,
-        OrderingTerm extends RepositoryOrderingTerm>
-    on AppDatabaseDaoRepository<DbItem, AppItem, DbId, TableDsl, TableInfoDsl,
-        Filters, OrderingTerm> {
+DbItem extends DataClass,
+DbPopulatedItem,
+AppItem,
+DbId,
+TableDsl extends Table,
+TableInfoDsl extends TableInfo<TableDsl, DbItem>,
+Filters,
+OrderingTerm extends RepositoryOrderingTerm>
+on AppDatabaseDaoRepository<DbItem, AppItem, DbId, TableDsl, TableInfoDsl,
+    Filters, OrderingTerm> {
   PopulatedDatabaseDaoMixin<DbItem, DbPopulatedItem, DbId, TableDsl,
       TableInfoDsl, Filters, OrderingTerm> get populatedDao;
 
   DbPopulatedItem mapAppItemToDbPopulatedItem(AppItem appItem);
 
   List<DbPopulatedItem> mapAppItemListToDbPopulatedItemList(
-          List<AppItem> appItems) =>
+      List<AppItem> appItems) =>
       appItems.map(mapAppItemToDbPopulatedItem).toList();
 
   DbPopulatedItem? mapAppItemToDbPopulatedItemNullable(AppItem? appItem) {
@@ -34,7 +34,7 @@ mixin PopulatedDatabaseDaoRepositoryMixin<
   AppItem mapDbPopulatedItemToAppItem(DbPopulatedItem dbPopulatedItem);
 
   List<AppItem> mapDbPopulatedItemListToAppItemList(
-          List<DbPopulatedItem> dbPopulatedItems) =>
+      List<DbPopulatedItem> dbPopulatedItems) =>
       dbPopulatedItems.map(mapDbPopulatedItemToAppItem).toList();
 
   AppItem? mapDbPopulatedItemToAppItemNullable(
@@ -76,12 +76,52 @@ mixin PopulatedDatabaseDaoRepositoryMixin<
       orderingTerms: orderingTerms,
     );
     var joinedQuery =
-        populatedDao.convertSimpleSelectStatementToJoinedSelectStatement(
+    populatedDao.convertSimpleSelectStatementToJoinedSelectStatement(
       query: query,
       filters: filters,
     );
 
     return joinedQuery;
+  }
+
+  @override
+  Future<int> findCount({
+    required Filters? filters,
+  }) async {
+    // todo: rework with COUNT * only
+    var query = dao.startSelectQuery();
+    dao.addFiltersToQuery(query: query, filters: filters);
+
+    // required because some filters added during join
+    var joinedQuery =
+    populatedDao.convertSimpleSelectStatementToJoinedSelectStatement(
+      query: query,
+      filters: filters,
+    );
+
+    var items = await joinedQuery.get();
+
+    return items.length;
+  }
+
+  @override
+  Stream<int> watchFindCount({
+    required Filters? filters,
+  }) {
+    // todo: rework with COUNT * only
+    var query = dao.startSelectQuery();
+    dao.addFiltersToQuery(query: query, filters: filters);
+
+    // required because some filters added during join
+    var joinedQuery =
+    populatedDao.convertSimpleSelectStatementToJoinedSelectStatement(
+      query: query,
+      filters: filters,
+    );
+
+    var stream = joinedQuery.watch();
+
+    return stream.map((items) => items.length);
   }
 
   @override
