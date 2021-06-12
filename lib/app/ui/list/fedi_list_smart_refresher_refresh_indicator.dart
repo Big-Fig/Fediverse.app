7bc009a4fcf31ed98cb0fc1c80730ae941bb2447
Yevhenii Zapletin <zapletin.yevhenii@gmail.com>
2021-06-12 15:39:31 +0300

    fix fedi_list_smart_refresher_refresh_indicator.dart: fix non-fatal when Scrollable in context is null

--- lib/app/ui/list/fedi_list_smart_refresher_refresh_indicator.dart
+++ lib/app/ui/list/fedi_list_smart_refresher_refresh_indicator.dart
@@ -66,7 +66,8 @@ class _FediListSmartRefresherRefreshIndicatorState
     );
     _valueAni.addListener(() {
       // frequently setState will decline the performance
-      if (mounted && Scrollable.of(context)!.position.pixels <= 0) {
+      var pixels = Scrollable.of(context)?.position.pixels;
+      if (mounted && pixels != null && pixels <= 0) {
         // ignore: no-empty-block
         setState(() {});
       }
