3b0e347518b70d23fb028cfe92da469ae7e99b8e
Yevhenii Zapletin <zapletin.yevhenii@gmail.com>
2021-07-24 16:27:01 +0300

    fix dart formatting

--- lib/app/auth/instance/join/join_auth_instance_widget_keys.dart
+++ lib/app/auth/instance/join/join_auth_instance_widget_keys.dart
@@ -1,10 +1,7 @@
 class JoinAuthInstanceWidgetKeys {
-  static const loginButtonKey =
-      'JoinAuthInstanceWidgetKeys_LoginButtonKey';
-  static const signUpButtonKey =
-      'JoinAuthInstanceWidgetKeys_SignUpButtonKey';
+  static const loginButtonKey = 'JoinAuthInstanceWidgetKeys_LoginButtonKey';
+  static const signUpButtonKey = 'JoinAuthInstanceWidgetKeys_SignUpButtonKey';
   static const exploreAsGuestButtonKey =
       'JoinAuthInstanceWidgetKeys_ExploreAsGuestButtonKey';
-  static const hostTextFieldKey =
-      'JoinAuthInstanceWidgetKeys_HostTextFieldKey';
+  static const hostTextFieldKey = 'JoinAuthInstanceWidgetKeys_HostTextFieldKey';
 }
