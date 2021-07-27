deba006a12f93af3d4cabbf4f6d753aa86363fc5
Yevhenii Zapletin <zapletin.yevhenii@gmail.com>
2021-07-27 17:55:53 +0300

    add dartfmt.sh tool + githook example

--- lib/pagination/cached/with_new_items/cached_pagination_list_with_new_items_bloc_impl.dart
+++ lib/pagination/cached/with_new_items/cached_pagination_list_with_new_items_bloc_impl.dart
@@ -219,7 +219,6 @@ abstract class CachedPaginationListWithNewItemsBloc<
       ],
     );
     unmergedNewItemsSubject.add([]);
-
   }
 
   @override
---
+++ tools/dartfmt.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+# Inspired by https://robots.thoughtbot.com/use-git-hooks-to-automate-annoying-tasks
+
+dart_files=$(git diff --cached --name-only --diff-filter=ACM | grep '.dart$')
+
+[ -z "$dart_files" ] && exit 0
+
+function checkfmt() {
+  unformatted=$(dartfmt -n $dart_files)
+  [ -z "$unformatted" ] && return 0
+
+  echo >&2 "Dart files must be formatted with dartfmt. Please run:"
+  for fn in $unformatted; do
+    echo >&2 "  dartfmt -w $PWD/$fn"
+  done
+
+  return 1
+}
+
+checkfmt || fail=yes
+
+[ -z "$fail" ] || exit 1
+
+echo 'all okay?'
+
+exit 0
