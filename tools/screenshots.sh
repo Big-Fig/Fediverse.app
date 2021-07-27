7d64e39467942c430ed99d3a50239c5826488892
Yevhenii Zapletin <zapletin.yevhenii@gmail.com>
2021-07-27 14:20:16 +0300

    delete unnecessary unframed images in frameit and screenshots tools

--- tools/screenshots.sh
+++ tools/screenshots.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
 fvm flutter pub global run screenshots:main --flavor dev
 #fvm flutter pub global run screenshots:main --flavor dev -c screenshots_dev.yaml
+# delete unframed unnecessary screenshots
+rm -rf ./ios/fastlane/unframed_screenshots
+find android/fastlane/metadata/android -type d -name "unframed_images" -exec rm -rf {} \;
