d199dbcd782d691cf4bee013f95e29c134e4a7de
Yevhenii Zapletin <zapletin.yevhenii@gmail.com>
2021-07-30 15:23:24 +0300

    fix frame_screenshots_for_fastlane.sh

--- tools/frame_screenshots_for_fastlane.sh
+++ tools/frame_screenshots_for_fastlane.sh
@@ -3,9 +3,9 @@ cd android/fastlane/
 bundle exec fastlane screenshots
 # delete all old png screenshots after framing
 find ./ -type f -name "*.png" ! -exec grep -q 'framed' {} \; -exec rm -rf {} \;
-cd ../../../../
+cd ../../
 cd ios/fastlane/
 bundle exec fastlane screenshots
 # delete all old png screenshots after framing
 find ./ -type f -name "*.png" ! -exec grep -q '_framed' {} \; -exec rm -rf {} \;
-cd ../../../
+cd ../../
