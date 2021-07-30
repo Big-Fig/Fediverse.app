4e55baa8604500c9f70819422064743ded8ea32b
Yevhenii Zapletin <zapletin.yevhenii@gmail.com>
2021-07-30 14:59:07 +0300

    improve screenshots.sh: dont build each time

--- tools/screenshots.sh
+++ tools/screenshots.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-fvm flutter pub global run screenshots:main --flavor dev
-#fvm flutter pub global run screenshots:main --flavor dev -c screenshots_dev.yaml
+fvm flutter pub global run screenshots:main --flavor dev -b false
+#fvm flutter pub global run screenshots:main --flavor dev -c screenshots_dev.yaml -b false
 # delete unframed unnecessary screenshots
 rm -rf ./ios/fastlane/unframed_screenshots
 find android/fastlane/metadata/android -type d -name "unframed_images" -exec rm -rf {} \;
