0236eba04cbc87d55ca555efff8c405b6071d353
Yevhenii Zapletin <zapletin.yevhenii@gmail.com>
2021-09-23 22:25:14 +0300

    fix auth_instance_model_test.dart

--- test/app/auth/instance/auth_instance_model_test.dart
+++ test/app/auth/instance/auth_instance_model_test.dart
@@ -1,69 +1,60 @@
-// import 'package:fediverse_api/fediverse_api.dart';
-// import 'package:flutter_test/flutter_test.dart';
-//
-// import '../../../hive/hive_test_helper.dart';
-// import '../../../json/json_test_helper.dart';
-// import '../../../obj/obj_test_helper.dart';
-// import 'auth_instance_model_test_helper.dart';
-// import 'package:unifedi_api/unifedi_api.dart';
-// import 'package:fediverse_api/fediverse_api.dart';
-//
-// void main() {
-//   test('equal & hashcode & toString', () async {
-//     ObjMockHelper.testEqualsHashcodeToString(
-//       ({required String seed}) =>
-//           UnifediApiAccessModelMockHelper.createTestUnifediApiAccess(
-//         seed: seed,
-//       ),
-//     );
-//   });
-//
-//   test('toJson & fromJson', () async {
-//     JsonMockHelper.testFromJsonToJson(
-//       ({required String seed}) =>
-//           UnifediApiAccessModelMockHelper.createTestUnifediApiAccess(
-//         seed: seed,
-//       ),
-//       (json) =>UnifediApiAccess.fromJson(json),
-//     );
-//   });
-//
-//   test('hive save&load', () async {
-//     await HiveMockHelper.testHiveSaveAndLoad(
-//       ({required String seed}) =>
-//           UnifediApiAccessModelMockHelper.createTestUnifediApiAccess(
-//         seed: seed,
-//       ),
-//     );
-//   });
-//
-//   test('copyWith', () async {
-//     var obj1 = UnifediApiAccessModelMockHelper.createTestUnifediApiAccess(
-//       seed: 'seed1',
-//     );
-//     var obj2 = UnifediApiAccessModelMockHelper.createTestUnifediApiAccess(
-//       seed: 'seed2',
-//     );
-//
-//     var obj2Obj1CopyWith = obj1.copyWith(
-//       urlSchema: obj2.urlSchema,
-//       urlHost: obj2.urlHost,
-//       acct: obj2.acct,
-//       token: obj2.token,
-//       authCode: obj2.authCode,
-//       isPleroma: obj2.isPleroma,
-//       application: obj2.application,
-//       info: obj2.info,
-//     );
-//
-//     expect(obj1 == obj2, false);
-//     expect(obj2, obj2Obj1CopyWith);
-//     expect(obj1, obj1.copyWith());
-//   });
-//
-//   test('hive adapter', () async {
-//     HiveMockHelper.testAdapter(
-//       () => UnifediApiAccessAdapter(),
-//     );
-//   });
-// }
+import 'package:flutter_test/flutter_test.dart';
+import 'package:unifedi_api/unifedi_api.dart';
+import 'package:unifedi_api/unifedi_api_mock_helper.dart';
+
+import '../../../hive/hive_test_helper.dart';
+import '../../../json/json_test_helper.dart';
+import '../../../obj/obj_test_helper.dart';
+
+void main() {
+  test('equal & hashcode & toString', () async {
+    ObjMockHelper.testEqualsHashcodeToString(
+      ({required String seed}) => UnifediApiAccessMockHelper.generate(
+        seed: seed,
+      ),
+    );
+  });
+
+  test('toJson & fromJson', () async {
+    JsonMockHelper.testFromJsonToJson(
+      ({required String seed}) => UnifediApiAccessMockHelper.generate(
+        seed: seed,
+      ),
+      (json) => UnifediApiAccess.fromJson(json),
+    );
+  });
+
+  test('hive save&load', () async {
+    await HiveMockHelper.testHiveSaveAndLoad(
+      ({required String seed}) => UnifediApiAccessMockHelper.generate(
+        seed: seed,
+      ),
+    );
+  });
+
+  test('copyWith', () async {
+    var obj1 = UnifediApiAccessMockHelper.generate(
+      seed: 'seed1',
+    );
+    var obj2 = UnifediApiAccessMockHelper.generate(
+      seed: 'seed2',
+    );
+
+    var obj2Obj1CopyWith = obj1.copyWith(
+      url: obj2.url,
+      instance: obj2.instance,
+      applicationAccessToken: obj2.applicationAccessToken,
+      userAccessToken: obj2.userAccessToken,
+    );
+
+    expect(obj1 == obj2, false);
+    expect(obj2, obj2Obj1CopyWith);
+    expect(obj1, obj1.copyWith());
+  });
+
+  test('hive adapter', () async {
+    HiveMockHelper.testAdapter(
+      () => UnifediApiAccessAdapter(),
+    );
+  });
+}
