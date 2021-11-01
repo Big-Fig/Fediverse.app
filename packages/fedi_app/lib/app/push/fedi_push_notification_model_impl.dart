b4a7f5e2bf5e00cc1a5641834f3daf00ff08248f
Yevhenii Zapletin <zapletin.yevhenii@gmail.com>
2021-11-01 23:59:39 +0300

    fix(fedi): add FediPushNotification hive adapter to fix push notifications for non-current instance

--- packages/fedi_app/lib/app/push/fedi_push_notification_model_impl.dart
+++ packages/fedi_app/lib/app/push/fedi_push_notification_model_impl.dart
@@ -1,5 +1,6 @@
 import 'package:fedi_app/app/push/fedi_push_notification_model.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:hive/hive.dart';
 import 'package:json_annotation/json_annotation.dart';
 import 'package:unifedi_api/unifedi_api.dart';
 
@@ -7,18 +8,29 @@ part 'fedi_push_notification_model_impl.freezed.dart';
 
 part 'fedi_push_notification_model_impl.g.dart';
 
+// ignore_for_file: no-magic-number
+
 @freezed
+@HiveType(typeId: -32 + 118)
 class FediPushNotification
     with _$FediPushNotification
     implements IFediPushNotification {
   const factory FediPushNotification({
-    required String account,
-    @JsonKey(name: 'notification_action') required String? notificationAction,
+    @HiveField(0) required String account,
+    @HiveField(1)
+    @JsonKey(name: 'notification_action')
+        required String? notificationAction,
+    @HiveField(2)
     @JsonKey(name: 'notification_action_input')
         required String? notificationActionInput,
-    @JsonKey(name: 'notification_id') required String notificationId,
-    @JsonKey(name: 'notification_type') required String notificationType,
-    required String server,
+    @HiveField(3)
+    @JsonKey(name: 'notification_id')
+        required String notificationId,
+    @HiveField(4)
+    @JsonKey(name: 'notification_type')
+        required String notificationType,
+    @HiveField(5) required String server,
+    @HiveField(6)
     @JsonKey(name: 'notification')
         required UnifediApiNotification? unifediApiNotification,
   }) = _FediPushNotification;
--- packages/fedi_app/lib/app/push/fedi_push_notification_model_impl.g.dart
+++ packages/fedi_app/lib/app/push/fedi_push_notification_model_impl.g.dart
@@ -3,6 +3,62 @@
 part of 'fedi_push_notification_model_impl.dart';
 
 // **************************************************************************
+// TypeAdapterGenerator
+// **************************************************************************
+
+class FediPushNotificationAdapter extends TypeAdapter<FediPushNotification> {
+  @override
+  final int typeId = 86;
+
+  @override
+  FediPushNotification read(BinaryReader reader) {
+    final numOfFields = reader.readByte();
+    final fields = <int, dynamic>{
+      for (int i = 0; i < numOfFields; i++) reader.readByte(): reader.read(),
+    };
+    return FediPushNotification(
+      account: fields[0] as String,
+      notificationAction: fields[1] as String?,
+      notificationActionInput: fields[2] as String?,
+      notificationId: fields[3] as String,
+      notificationType: fields[4] as String,
+      server: fields[5] as String,
+      unifediApiNotification: fields[6] as UnifediApiNotification?,
+    );
+  }
+
+  @override
+  void write(BinaryWriter writer, FediPushNotification obj) {
+    writer
+      ..writeByte(7)
+      ..writeByte(3)
+      ..write(obj.notificationId)
+      ..writeByte(5)
+      ..write(obj.server)
+      ..writeByte(0)
+      ..write(obj.account)
+      ..writeByte(4)
+      ..write(obj.notificationType)
+      ..writeByte(6)
+      ..write(obj.unifediApiNotification)
+      ..writeByte(1)
+      ..write(obj.notificationAction)
+      ..writeByte(2)
+      ..write(obj.notificationActionInput);
+  }
+
+  @override
+  int get hashCode => typeId.hashCode;
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      other is FediPushNotificationAdapter &&
+          runtimeType == other.runtimeType &&
+          typeId == other.typeId;
+}
+
+// **************************************************************************
 // JsonSerializableGenerator
 // **************************************************************************
 
