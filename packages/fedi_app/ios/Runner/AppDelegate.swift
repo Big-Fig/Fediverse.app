9f56b335f4b6392926e30b940451273a0839bb4a
Josh Soref <2119212+jsoref@users.noreply.github.com>
2022-11-08 19:38:03 -0500

    spelling: library
    Signed-off-by: Josh Soref <2119212+jsoref@users.noreply.github.com>

--- packages/fedi_app/ios/Runner/AppDelegate.swift
+++ packages/fedi_app/ios/Runner/AppDelegate.swift
@@ -18,7 +18,7 @@ import receive_sharing_intent
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 
-  // If the application is using multiple libraries, which needs to implement this function here in AppDelegate, you should check if the url is made from SwiftReceiveSharingIntentPlugin (if so, return the sharingIntent response) or call the handler of specific librabry
+  // If the application is using multiple libraries, which needs to implement this function here in AppDelegate, you should check if the url is made from SwiftReceiveSharingIntentPlugin (if so, return the sharingIntent response) or call the handler of specific library
   override func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey : Any] = [:]) -> Bool {
       let sharingIntent = SwiftReceiveSharingIntentPlugin.instance
       if sharingIntent.hasMatchingSchemePrefix(url: url) {
